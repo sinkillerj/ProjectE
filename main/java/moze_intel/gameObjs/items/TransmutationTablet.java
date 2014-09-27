9d80ca85ed1878adfabc20cca83c7b89ab92c781
Kolatra <t.louks88@gmail.com>
2014-09-27 07:08:02 -0600

    Forgot to add TransmutationTablet class.

---
+++ main/java/moze_intel/gameObjs/items/TransmutationTablet.java
@@ -0,0 +1,37 @@
+package moze_intel.gameObjs.items;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import moze_intel.MozeCore;
+import moze_intel.utils.Constants;
+import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+public class TransmutationTablet extends ItemBase
+{
+    public TransmutationTablet()
+    {
+        super();
+        this.setUnlocalizedName("transtablet");
+        this.setMaxStackSize(1);
+    }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player)
+    {
+        if (!world.isRemote)
+        {
+            player.openGui(MozeCore.instance, Constants.RM_FURNACE_GUI, world, (int) player.posX, (int) player.posY, (int) player.posZ);
+        }
+        return stack;
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerIcons(IIconRegister register)
+    {
+        this.itemIcon = register.registerIcon(this.getTexture("transtablet"));
+    }
+}
