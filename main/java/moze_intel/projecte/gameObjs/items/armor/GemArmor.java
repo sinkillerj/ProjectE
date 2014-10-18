10dfc3f6d8ee876961ef83e942460f1fde8bd8bd
Kolatra <t.louks88@gmail.com>
2014-10-18 13:50:23 -0600

    Added Thaumcraft API
    Gem Armor Helmet now has Goggles of Revealing functionality.

--- main/java/moze_intel/projecte/gameObjs/items/armor/GemArmor.java
+++ main/java/moze_intel/projecte/gameObjs/items/armor/GemArmor.java
@@ -30,8 +30,10 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.ISpecialArmor;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import thaumcraft.api.IGoggles;
+import thaumcraft.api.nodes.IRevealer;
 
-public class GemArmor extends ItemArmor implements ISpecialArmor
+public class GemArmor extends ItemArmor implements ISpecialArmor, IGoggles, IRevealer
 {
 	public GemArmor(int armorType)
 	{
@@ -41,8 +43,23 @@ public class GemArmor extends ItemArmor implements ISpecialArmor
 		this.setHasSubtypes(false);
 		this.setMaxDamage(0);
 	}
-	
-	@Override
+
+    @Override
+    public boolean showIngamePopups(ItemStack itemstack, EntityLivingBase player) {
+        if (itemstack != null && itemstack.getItem() == ObjHandler.gemHelmet)
+             return true;
+        return false;
+    }
+
+    @Override
+    public boolean showNodes(ItemStack itemstack, EntityLivingBase player)
+    {
+        if (itemstack != null && itemstack.getItem() == ObjHandler.gemHelmet)
+            return true;
+        return false;
+    }
+
+    @Override
     public void onArmorTick(World world, EntityPlayer player, ItemStack stack)
 	{
 		if (world.isRemote)
