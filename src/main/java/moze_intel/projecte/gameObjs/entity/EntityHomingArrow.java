1ddf3f1d76f10d4120fd707c91bf38fb467d7f85
Кирилл <kir@localhost.localdomain>
2015-01-27 14:14:17 +0400

    removed errorneous speed setting, made arrow home on nearest enemy, added tracing when homing

--- src/main/java/moze_intel/projecte/gameObjs/entity/EntityHomingArrow.java
+++ src/main/java/moze_intel/projecte/gameObjs/entity/EntityHomingArrow.java
@@ -8,17 +8,29 @@ import net.minecraft.world.World;
 
 import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Random;
 
 public class EntityHomingArrow extends EntityArrow
 {
+	EntityLiving target;
+	World world;
+
+	private void init(World world)
+	{
+		rng = new Random();
+		this.world = world;
+	}
+
 	public EntityHomingArrow(World world)
 	{
 		super(world);
+		init(world);
 	}
 
 	public EntityHomingArrow(World world, EntityLivingBase par2, float par3) 
 	{
 		super(world, par2, par3);
+		init(world);
 	}
 	
 	@Override
@@ -26,26 +38,59 @@ public class EntityHomingArrow extends EntityArrow
 	{
 		//TODO Create proper custom arrow. This one is duplicating because of the super call for onUpdate();
 		super.onUpdate();
+
+		AxisAlignedBB box = this.boundingBox;
 		
-		if (!isInGround())
+		if (target == null)
 		{
-			AxisAlignedBB bBox = this.boundingBox.expand(16, 16, 16);
+			AxisAlignedBB bBox = box.expand(8, 8, 8);
 			List<EntityLiving> list = this.worldObj.getEntitiesWithinAABB(EntityLiving.class, bBox);
+
+			double distance = 100000;
+
+			for (EntityLiving entity : list)
+			{
+				double toIt = distanceTo(entity);
+
+				if (distance > toIt)
+				{
+					distance = toIt;
+					target = entity;
+				}
+			}
 			
-			if (list.size() <= 0)
+			if (target == null)
 			{
 				return;
 			}
-			
-			EntityLiving target = list.get(0);
-			
+
 			double d5 = target.posX - this.posX;
-			double d6 = target.boundingBox.minY + (double)(target.height / 2.0F) - (this.posY + (double)(this.height / 2.0F));
+			double d6 = target.boundingBox.minY + target.height - this.posY;
 			double d7 = target.posZ - this.posZ;
 			
 			this.setThrowableHeading(d5, d6, d7, 2.0F, 0.0F);
-			this.setVelocity(d5, d6, d7);
 		}
+		else if (!isInGround())
+		{
+			world.spawnParticle("flame", box.maxX, box.maxY, box.maxZ, 0.0D, 0.0D, 0.0D);
+		}
+	}
+
+	private double distanceTo(EntityLiving entity)
+	{
+		double [] ds = new double []
+		{
+			this.posX - entity.posX,
+			this.posY - entity.posY,
+			this.posZ - entity.posZ
+		};
+
+		double d = 0;
+
+		for (int i = 0; i < 3; i++)
+			d += ds[i] * ds[i];
+
+		return Math.sqrt(d);
 	}
 	
 	private boolean isInGround()
