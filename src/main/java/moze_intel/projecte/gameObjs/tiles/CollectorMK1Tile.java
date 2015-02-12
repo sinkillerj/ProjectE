a6ef4d3fa867350baee0228a2056c5c91d6d4668
Vincent Lee <vincent_a_lee@outlook.com>
2015-02-11 19:19:45 -0600

    Fix space/tabs hopefully

--- src/main/java/moze_intel/projecte/gameObjs/tiles/CollectorMK1Tile.java
+++ src/main/java/moze_intel/projecte/gameObjs/tiles/CollectorMK1Tile.java
@@ -115,30 +115,30 @@ public class CollectorMK1Tile extends TileEmcProducer implements IInventory, ISi
 					&& inventory[upgradedSlot].getItem() == inventory[lockSlot].getItem()
 					&& inventory[upgradedSlot].stackSize < inventory[upgradedSlot].getMaxStackSize())) {
 				for (int i = 1; i < invBufferSize; i++)
-                {
-                    if (inventory[i] == null)
-                    {
-                        inventory[i] = inventory[upgradedSlot];
-                        inventory[upgradedSlot] = null;
-                        break;
-                    }
-                    else if (Utils.areItemStacksEqual(inventory[i], inventory[upgradedSlot]))
-                    {
-                        int remain = inventory[i].getMaxStackSize() - inventory[i].stackSize;
+				{
+					if (inventory[i] == null)
+					{
+						inventory[i] = inventory[upgradedSlot];
+						inventory[upgradedSlot] = null;
+						break;
+					}
+					else if (Utils.areItemStacksEqual(inventory[i], inventory[upgradedSlot]))
+					{
+						int remain = inventory[i].getMaxStackSize() - inventory[i].stackSize;
 
-                        if (remain >= inventory[upgradedSlot].stackSize)
-                        {
-                            inventory[i].stackSize += inventory[upgradedSlot].stackSize;
-                            inventory[upgradedSlot] = null;
-                            break;
-                        }
-                        else
-                        {
-                            inventory[i].stackSize += remain;
-                            inventory[upgradedSlot].stackSize -= remain;
-                        }
-                    }
-                }
+						if (remain >= inventory[upgradedSlot].stackSize)
+						{
+							inventory[i].stackSize += inventory[upgradedSlot].stackSize;
+							inventory[upgradedSlot] = null;
+							break;
+						}
+						else
+						{
+							inventory[i].stackSize += remain;
+							inventory[upgradedSlot].stackSize -= remain;
+						}
+					}
+				}
 			}
 		}
 		
