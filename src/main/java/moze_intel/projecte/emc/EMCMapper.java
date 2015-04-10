e1b74a452943e902d82a336a5f0012acda083fc0
artman41 <artman41@gmail.com>
2015-04-10 19:51:52 +0100

    Revert "Added New Mapped to EMCMapper"
    This reverts commit 821b3e50ec886737ff14100b09fdf8ee014ba7b9.

--- src/main/java/moze_intel/projecte/emc/EMCMapper.java
+++ src/main/java/moze_intel/projecte/emc/EMCMapper.java
@@ -19,7 +19,7 @@ public final class EMCMapper
 
 	public static void map()
 	{
-		List<IEMCMapper<NormalizedSimpleStack, Integer>> emcMappers = Arrays.asList(new OreDictionaryMapper(), new LazyMapper(), new CustomEMCMapper(), new CraftingMapper(), new FluidMapper(), new SmeltingMapper(), new ModMapper());
+		List<IEMCMapper<NormalizedSimpleStack, Integer>> emcMappers = Arrays.asList(new OreDictionaryMapper(), new LazyMapper(), new CustomEMCMapper(), new CraftingMapper(), new moze_intel.projecte.emc.mappers.FluidMapper(), new SmeltingMapper());
 		GraphMapper<NormalizedSimpleStack, Integer> graphMapper = new SimpleGraphMapper<NormalizedSimpleStack, Integer>(new IntArithmetic());
 
 		Configuration config = new Configuration(new File(PECore.CONFIG_DIR, "mapping.cfg"));
