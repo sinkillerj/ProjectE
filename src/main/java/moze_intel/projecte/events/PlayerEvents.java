80c03da2a607c74872e1a35e66fc3586f4360bc7
Vincent Lee <vincent.lee@outlook.com>
2015-04-12 11:49:02 -0500

    Oops

--- src/main/java/moze_intel/projecte/events/PlayerEvents.java
+++ src/main/java/moze_intel/projecte/events/PlayerEvents.java
@@ -45,7 +45,7 @@ public class PlayerEvents
 	@SubscribeEvent
 	public void onHighAlchemistJoin(cpw.mods.fml.common.gameevent.PlayerEvent.PlayerLoggedInEvent evt)
 	{
-		if (true)//PECore.uuids.contains((evt.player.getUniqueID().toString())))
+		if (PECore.uuids.contains((evt.player.getUniqueID().toString())))
 		{
 			IChatComponent prior = ChatHelper.modifyColor(new ChatComponentTranslation("pe.server.high_alchemist"), EnumChatFormatting.BLUE);
 			IChatComponent playername = ChatHelper.modifyColor(new ChatComponentText(" " + evt.player.getCommandSenderName() + " "), EnumChatFormatting.GOLD);
