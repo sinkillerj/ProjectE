6c62ea83643281abbffb00706e882ef5e952f2d5
MaPePeR <MaPePeR@users.noreply.github.com>
2015-05-04 20:00:17 +0200

    Config options...

--- src/main/java/moze_intel/projecte/emc/mappers/NEIMapper.java
+++ src/main/java/moze_intel/projecte/emc/mappers/NEIMapper.java
@@ -3,26 +3,21 @@ package moze_intel.projecte.emc.mappers;
 import moze_intel.projecte.emc.IMappingCollector;
 import moze_intel.projecte.emc.IngredientMap;
 import moze_intel.projecte.emc.NormalizedSimpleStack;
-import moze_intel.projecte.emc.SimpleStack;
 
 import codechicken.nei.PositionedStack;
-import codechicken.nei.recipe.GuiCraftingRecipe;
-import codechicken.nei.recipe.ICraftingHandler;
 import codechicken.nei.recipe.IRecipeHandler;
 import codechicken.nei.recipe.TemplateRecipeHandler;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.config.Configuration;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class NEIMapper implements IEMCMapper<NormalizedSimpleStack, Integer>
 {
@@ -81,35 +76,56 @@ public class NEIMapper implements IEMCMapper<NormalizedSimpleStack, Integer>
 						continue;
 					}
 					TemplateRecipeHandler trh = (TemplateRecipeHandler)recipeHandler;
-					if (config.getBoolean("enabled", getCategoryForRecipeHandler(trh), false, //Everything disabled by default
-							String.format("Has %s recipes for \"%s\" with identifier \"%s\"", trh.numRecipes(), trh.getRecipeName(), trh.getOverlayIdentifier())
-					))
+					try
 					{
-						doTemplateRecipeHandler(mapper, config, trh);
+						trh.loadCraftingRecipes(trh.getOverlayIdentifier());
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+					RecipeHandlerConfig  recipeHandlerConfig = RecipeHandlerConfig.fromCategory(config, trh);
+					if (recipeHandlerConfig.enabled)
+					{
+						doTemplateRecipeHandler(mapper, recipeHandlerConfig, trh);
 					}
 				}
 			}
 		} catch (Exception e) {
 			System.out.println("Could not load Recipes from NEI");
+			e.printStackTrace();
 		}
 	}
 
-	protected String getCategoryForRecipeHandler(TemplateRecipeHandler trh)
+	protected static String getCategoryForRecipeHandler(TemplateRecipeHandler trh)
 	{
 		return "recipeHandlers." + trh.getClass().getName().replace('.', '_');
 	}
 
-	protected void doTemplateRecipeHandler(IMappingCollector<NormalizedSimpleStack, Integer> mapper, Configuration config, TemplateRecipeHandler trh)
+	protected void doTemplateRecipeHandler(IMappingCollector<NormalizedSimpleStack, Integer> mapper, RecipeHandlerConfig config, TemplateRecipeHandler trh)
 	{
 		try {
-			trh.loadCraftingRecipes(trh.getOverlayIdentifier());
 			System.out.println(trh.numRecipes());
 			for (int recipeNumber = 0; recipeNumber < trh.numRecipes(); recipeNumber++) {
 				List<PositionedStack> ingredients = trh.getIngredientStacks(recipeNumber);
 				ItemStack outStack = trh.getResultStack(recipeNumber).item;
 				IngredientMap<NormalizedSimpleStack> ingredientsNSSMap = new IngredientMap<NormalizedSimpleStack>();
-				for (PositionedStack ingredient: ingredients)
+				if (config.log) {
+					StringBuilder sb = new StringBuilder();
+					for (int slotNum = 0; slotNum < ingredients.size(); slotNum++)
+					{
+						if (slotNum != 0) sb.append(" + ");
+						if (config.skipSlots.contains(slotNum + 1)) sb.append("(");
+						if (ingredients.get(slotNum).items.length > 1) sb.append("[");
+						sb.append(ingredients.get(slotNum).item);
+						if (ingredients.get(slotNum).items.length > 1) sb.append(", ..]");
+						if (config.skipSlots.contains(slotNum + 1)) sb.append(")");
+					}
+					sb.append(" = ").append(outStack);
+					System.out.println(sb.toString());
+				}
+				for (int slotNum = 0; slotNum < ingredients.size(); slotNum++)
 				{
+					if (config.skipSlots.contains(slotNum + 1)) continue;
+					PositionedStack ingredient = ingredients.get(slotNum);
 					NormalizedSimpleStack ingredientNSS;
 					if (ingredient.items.length == 1) {
 						ingredientNSS = NormalizedSimpleStack.getNormalizedSimpleStackFor(ingredient.items[0]);
@@ -126,10 +142,42 @@ public class NEIMapper implements IEMCMapper<NormalizedSimpleStack, Integer>
 					}
 
 				}
-				mapper.addConversionMultiple(outStack.stackSize, NormalizedSimpleStack.getNormalizedSimpleStackFor(outStack), ingredientsNSSMap.getMap());
+				if (config.log)
+				{
+					ingredientsNSSMap.toString();
+				}
+				mapper.addConversionMultiple(outStack.stackSize*config.outputMultiplier, NormalizedSimpleStack.getNormalizedSimpleStackFor(outStack), ingredientsNSSMap.getMap());
 			}
 		} catch (Exception e) {
 			System.out.println("Could not get Recipes from IRecipeHandler" + trh.toString());
+		}
+	}
+
+
+	Map<String, RecipeHandlerConfig> config = Maps.newHashMap();
+	protected static class RecipeHandlerConfig
+	{
+		private RecipeHandlerConfig() {}
+		public boolean enabled;
+		public Set<Integer> skipSlots;
+		public int outputMultiplier;
+		public boolean log;
+		public static RecipeHandlerConfig fromCategory(Configuration config, TemplateRecipeHandler trh)
+		{
+			String category = getCategoryForRecipeHandler(trh);
+			RecipeHandlerConfig c = new RecipeHandlerConfig();
+			c.enabled = config.getBoolean("enabled", category, false,
+					String.format("Has %s recipes for \"%s\" with identifier \"%s\"", trh.numRecipes(), trh.getRecipeName(), trh.getOverlayIdentifier())
+			);
+			c.outputMultiplier = config.getInt("outputMultiplier", category, 1, 1, 128, "Increase the amount gained.");
+			c.log = config.getBoolean("logging", category, false, "Enable extensive logging for this RecipeHandler");
+			int[] skipSlots = config.get(category, "skipSlots", new int[]{}, "").getIntList();
+			c.skipSlots = Sets.newHashSet();
+			for (int i = 0; i < skipSlots.length; i++)
+			{
+				c.skipSlots.add(skipSlots[i]);
+			}
+			return c;
 		}
 	}
 }
