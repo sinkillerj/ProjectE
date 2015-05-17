8afd01c6d4cfa88152f9d20f70e298070f49fb51
MaPePeR <MaPePeR@users.noreply.github.com>
2015-05-17 11:41:09 +0200

    Optimize imports.

--- src/main/java/moze_intel/projecte/network/commands/ProjectECMD.java
+++ src/main/java/moze_intel/projecte/network/commands/ProjectECMD.java
@@ -1,23 +1,14 @@
 package moze_intel.projecte.network.commands;
 
-import moze_intel.projecte.config.CustomEMCParser;
-import moze_intel.projecte.emc.ThreadReloadEMCMap;
-import moze_intel.projecte.utils.ChatHelper;
-import moze_intel.projecte.utils.MathUtils;
-
 import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import net.minecraft.command.ICommandSender;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ChatComponentTranslation;
-import net.minecraft.util.EnumChatFormatting;
 
 import java.util.Arrays;
 import java.util.List;
-
-import com.google.common.collect.Lists;
 
 public class ProjectECMD extends ProjectEBaseCMD
 {
