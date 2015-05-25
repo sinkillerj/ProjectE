c6023008b41045390704a6ce711cddf25bc73f68
Vincent Lee <vincent.lee@outlook.com>
2015-05-25 10:51:12 -0500

    Space->tab

--- src/main/java/moze_intel/projecte/utils/ComparatorHelper.java
+++ src/main/java/moze_intel/projecte/utils/ComparatorHelper.java
@@ -16,50 +16,48 @@ import net.minecraft.world.World;
  */
 public final class ComparatorHelper
 {
-    public static int getForAlchChest(World world, int x, int y, int z)
-    {
-        return Container.calcRedstoneFromInventory(((AlchChestTile) world.getTileEntity(x, y, z)));
-    }
+	public static int getForAlchChest(World world, int x, int y, int z)
+	{
+		return Container.calcRedstoneFromInventory(((AlchChestTile) world.getTileEntity(x, y, z)));
+	}
 
-    public static int getForCollector(World world, int x, int y, int z)
-    {
-        CollectorMK1Tile tile = ((CollectorMK1Tile) world.getTileEntity(x, y, z));
-        ItemStack charging = tile.getChargingItem();
-        if (charging != null)
-        {
-            if (charging.getItem() == ObjHandler.kleinStars)
-            {
-                double max = EMCHelper.getKleinStarMaxEmc(charging);
-                double current = ItemPE.getEmc(charging);
-                return MathUtils.scaleToRedstone(current, max);
-            }
-            else
-            {
+	public static int getForCollector(World world, int x, int y, int z)
+	{
+		CollectorMK1Tile tile = ((CollectorMK1Tile) world.getTileEntity(x, y, z));
+		ItemStack charging = tile.getChargingItem();
+		if (charging != null)
+		{
+			if (charging.getItem() == ObjHandler.kleinStars)
+			{
+				double max = EMCHelper.getKleinStarMaxEmc(charging);
+				double current = ItemPE.getEmc(charging);
+				return MathUtils.scaleToRedstone(current, max);
+			} else
+			{
 
-                double needed = tile.getEmcToNextGoal();
-                double current = tile.getStoredEmc();
-                return MathUtils.scaleToRedstone(current, needed);
-            }
-        }
-        else
-        {
-            return MathUtils.scaleToRedstone(tile.getStoredEmc(), tile.getMaxEmc());
-        }
-    }
+				double needed = tile.getEmcToNextGoal();
+				double current = tile.getStoredEmc();
+				return MathUtils.scaleToRedstone(current, needed);
+			}
+		} else
+		{
+			return MathUtils.scaleToRedstone(tile.getStoredEmc(), tile.getMaxEmc());
+		}
+	}
 
-    public static int getForCondenser(World world, int x, int y, int z)
-    {
-        return Container.calcRedstoneFromInventory(((CondenserTile) world.getTileEntity(x, y, z)));
-    }
+	public static int getForCondenser(World world, int x, int y, int z)
+	{
+		return Container.calcRedstoneFromInventory(((CondenserTile) world.getTileEntity(x, y, z)));
+	}
 
-    public static int getForMatterFurnace(World world, int x, int y, int z)
-    {
-        return Container.calcRedstoneFromInventory(((RMFurnaceTile) world.getTileEntity(x, y, z)));
-    }
+	public static int getForMatterFurnace(World world, int x, int y, int z)
+	{
+		return Container.calcRedstoneFromInventory(((RMFurnaceTile) world.getTileEntity(x, y, z)));
+	}
 
-    public static int getForRelay(World world, int x, int y, int z)
-    {
-        RelayMK1Tile relay = ((RelayMK1Tile) world.getTileEntity(x, y, z));
-        return MathUtils.scaleToRedstone(relay.getStoredEmc(), relay.getMaxEmc());
-    }
+	public static int getForRelay(World world, int x, int y, int z)
+	{
+		RelayMK1Tile relay = ((RelayMK1Tile) world.getTileEntity(x, y, z));
+		return MathUtils.scaleToRedstone(relay.getStoredEmc(), relay.getMaxEmc());
+	}
 }
