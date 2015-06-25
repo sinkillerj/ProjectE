19d556d81757eb5cba6eb15d1f9d84d7b17ca2b2
MaPePeR <MaPePeR@users.noreply.github.com>
2015-06-25 21:06:01 +0200

    Test for hidden fraction propagation.
    Taken from Reliquary's Lilypads of Fertility

--- src/test/java/moze_intel/projecte/emc/HiddenFractionSpecificTest.java
+++ src/test/java/moze_intel/projecte/emc/HiddenFractionSpecificTest.java
@@ -80,6 +80,27 @@ public class HiddenFractionSpecificTest
 
 	}
 
+
+	@Test
+	public void fractionPropagation()
+	{
+		graphMapper.setValue("glass", 1, IMappingCollector.FixedValue.FixAndInherit);
+
+		graphMapper.addConversionMultiple(16, "pane", ImmutableMap.of("glass", 6));
+		graphMapper.addConversionMultiple(5, "vial", ImmutableMap.of("pane", 5));
+		//Internal EMC of pane and vial: 3/8 = 0.375
+		//So 8 * vial should have an emc of 3 => testItem should have emc of 1
+		graphMapper.addConversionMultiple(3, "testItem1", ImmutableMap.of("pane", 8));
+		graphMapper.addConversionMultiple(3, "testItem2", ImmutableMap.of("vial", 8));
+
+		Map<String, Integer> values = graphMapper.generateValues();
+		assertEquals(1, getValue(values, "glass"));
+		assertEquals(0, getValue(values, "pane"));
+		assertEquals(0, getValue(values, "vial"));
+		assertEquals(1, getValue(values, "testItem1"));
+		assertEquals(1, getValue(values, "testItem2"));
+	}
+
 	private static <T, V extends Number> int getValue(Map<T, V> map, T key) {
 		V val = map.get(key);
 		if (val == null) return 0;
