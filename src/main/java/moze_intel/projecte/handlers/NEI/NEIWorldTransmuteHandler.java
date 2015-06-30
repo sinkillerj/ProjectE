76170ff0bc86e79bd9c8001e87e4d9309ed8f292
Dwargkiller <peter.hammerskog@gmail.com>
2015-06-30 18:08:11 +0200

    Finished GUI

--- src/main/java/moze_intel/projecte/handlers/NEI/NEIWorldTransmuteHandler.java
+++ src/main/java/moze_intel/projecte/handlers/NEI/NEIWorldTransmuteHandler.java
@@ -1,8 +1,11 @@
 package moze_intel.projecte.handlers.NEI;
 
+import java.awt.Rectangle;
 import java.util.List;
 import java.util.Map.Entry;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.util.ResourceLocation;
@@ -14,6 +17,8 @@ import codechicken.nei.api.DefaultOverlayRenderer;
 import codechicken.nei.recipe.TemplateRecipeHandler;
 import moze_intel.projecte.utils.MetaBlock;
 import moze_intel.projecte.utils.WorldTransmutations;
+import moze_intel.projecte.gameObjs.ObjHandler;
+import moze_intel.projecte.gameObjs.items.PhilosophersStone;
 
 public class NEIWorldTransmuteHandler extends TemplateRecipeHandler {
 
@@ -27,18 +32,19 @@ public class NEIWorldTransmuteHandler extends TemplateRecipeHandler {
 
 	@Override
 	public String getGuiTexture() {
-		return "nei:textures/gui/recipebg.png";
+		return "projecte:textures/gui/nei.png";
 	}
 	
 	public class CachedTransmutationRecipe extends CachedRecipe{
 		
 		private MetaBlock input;
 		private MetaBlock output;
-		private MetaBlock[] inputs;
+		public boolean sneaking;
 		
-		public CachedTransmutationRecipe(MetaBlock in,boolean sneaking) {
+		public CachedTransmutationRecipe(MetaBlock in,boolean sneak) {
 			
 			input = in;
+			sneaking = sneak;
 			output = WorldTransmutations.getWorldTransmutation(in,sneaking);
 			
 		}
@@ -46,7 +52,7 @@ public class NEIWorldTransmuteHandler extends TemplateRecipeHandler {
 		@Override
 		public PositionedStack getIngredient() {
 			ItemStack is = input.toItemStack();
-			return new PositionedStack(is,21,29);
+			return new PositionedStack(is,22,23);
 			
 		}
 		
@@ -55,9 +61,14 @@ public class NEIWorldTransmuteHandler extends TemplateRecipeHandler {
 		@Override
 		public PositionedStack getResult() {
 			ItemStack result = output.toItemStack();
-			return new PositionedStack(result,128,29);
+			return new PositionedStack(result,128,23);
 			
 		}
+		
+	    @Override
+	    public PositionedStack getOtherStack(){
+	    	return new PositionedStack(new ItemStack(ObjHandler.philosStone), 60, 23);
+	    }
 		
 	}
 	
@@ -65,7 +76,10 @@ public class NEIWorldTransmuteHandler extends TemplateRecipeHandler {
     public void loadCraftingRecipes(String outputId, Object... results) {
         if (outputId.equals(id) && getClass() == NEIWorldTransmuteHandler.class) {
             for (Entry<MetaBlock, MetaBlock[]> entry: WorldTransmutations.MAP.entrySet()) {
-            	if(entry != null && entry.getValue()!= null) arecipes.add(new CachedTransmutationRecipe(entry.getKey(),false));
+            	if(entry!=null){
+            		if(entry.getValue()[0]!= null) arecipes.add(new CachedTransmutationRecipe(entry.getKey(), false));
+            		if(entry.getValue()[1]!=null) arecipes.add(new CachedTransmutationRecipe(entry.getKey(), true));
+            	}
             }
         } else {
             super.loadCraftingRecipes(outputId, results);
@@ -89,11 +103,31 @@ public class NEIWorldTransmuteHandler extends TemplateRecipeHandler {
     		if(NEIServerUtils.areStacksSameTypeCrafting(entry.getKey().toItemStack(), ingredient)) {
             	if(entry!=null){
             		if(entry.getValue()[0]!= null) arecipes.add(new CachedTransmutationRecipe(entry.getKey(), false));
-            		else if(entry.getValue()[1]!=null) arecipes.add(new CachedTransmutationRecipe(entry.getKey(), true));
+            		if(entry.getValue()[1]!=null) arecipes.add(new CachedTransmutationRecipe(entry.getKey(), true));
             	}
             }
         }
     }
+    
+    @Override
+	public void drawForeground( int recipe )
+	{
+			String sneak = StatCollector.translateToLocal("key.sneak");
+			
+			CachedTransmutationRecipe r = (CachedTransmutationRecipe) arecipes.get(recipe);
+			
+			FontRenderer fr = Minecraft.getMinecraft().fontRenderer;
+			if(r.sneaking)fr.drawString( sneak, 70, 40,0);
+
+	}
+    
+
+    @Override
+    public void loadTransferRects(){
+    	this.transferRects.add(new TemplateRecipeHandler.RecipeTransferRect(new Rectangle(83,23,25,10), id, new Object[0]));
+    }
+    
+
 
 	
 	
