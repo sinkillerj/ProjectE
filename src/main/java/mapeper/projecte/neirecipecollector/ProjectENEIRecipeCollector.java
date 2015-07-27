f22fd186a7f2c2457f82d4046f7a5f8a0224c454
MaPePeR <MaPePeR@users.noreply.github.com>
2015-07-27 18:42:14 +0200

    .gitignore and proper mod name

--- src/main/java/mapeper/projecte/neirecipecollector/ProjectENEIRecipeCollector.java
+++ src/main/java/mapeper/projecte/neirecipecollector/ProjectENEIRecipeCollector.java
@@ -1,14 +1,14 @@
-package com.example.examplemod;
+package mapeper.projecte.neirecipecollector;
 
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 
-@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = ProjectENEIRecipeCollector.MODID, version = ProjectENEIRecipeCollector.VERSION)
+public class ProjectENEIRecipeCollector
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "ProjectENEIRecipeCollector";
     public static final String VERSION = "1.0";
     
     @EventHandler
