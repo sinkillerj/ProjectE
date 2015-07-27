a9a29c506edc388ac76a069a4823042e8c6dca9c
MaPePeR <MaPePeR@users.noreply.github.com>
2015-07-27 23:15:13 +0200

    More logging and formatting options

--- src/main/java/mapeper/projecte/neirecipecollector/commands/CollectSubCommand.java
+++ src/main/java/mapeper/projecte/neirecipecollector/commands/CollectSubCommand.java
@@ -1,18 +1,29 @@
 package mapeper.projecte.neirecipecollector.commands;
 
+import codechicken.nei.PositionedStack;
 import codechicken.nei.recipe.IRecipeHandler;
 import codechicken.nei.recipe.TemplateRecipeHandler;
 import com.google.common.base.Splitter;
 import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import cpw.mods.fml.common.registry.GameRegistry;
 import mapeper.projecte.neirecipecollector.ChatUtils;
 import mapeper.projecte.neirecipecollector.LowerCasePrefixPredicate;
 import mapeper.projecte.neirecipecollector.NEIRecipeCollector;
+import mapeper.projecte.neirecipecollector.ProjectENEIRecipeCollector;
 import net.minecraft.command.ICommandSender;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.StatCollector;
+import org.apache.logging.log4j.Logger;
+import scala.actors.threadpool.Arrays;
 
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class CollectSubCommand implements ISubCommand
 {
+	private Logger LOGGER = ProjectENEIRecipeCollector.logger;
 	@Override
 	public String getCommandName()
 	{
@@ -29,9 +40,12 @@ public class CollectSubCommand implements ISubCommand
 		return null;
 	}
 
+
+
 	@Override
 	public void processCommand(String previousCommands, ICommandSender sender, List<String> params)
 	{
+		resetSettings();
 		if (params.size() == 0) {
 			ChatUtils.addChatError(sender, "Usage: '%s <IRecipeHandler class name> [options]'", previousCommands);
 			ChatUtils.addChatError(sender, "Options: ");
@@ -39,6 +53,8 @@ public class CollectSubCommand implements ISubCommand
 			ChatUtils.addChatError(sender, "multiplier=<n>   Multiply the output stacksize");
 			ChatUtils.addChatError(sender, "some             Show the first 3 recipes");
 			ChatUtils.addChatError(sender, "log              Log all recipes");
+			ChatUtils.addChatError(sender, "unlocal          Log unique item names instead display name");
+			ChatUtils.addChatError(sender, "fulllist         Don't abbreviate ingredient options");
 			return;
 		}
 
@@ -49,27 +65,39 @@ public class CollectSubCommand implements ISubCommand
 			return;
 		}
 
-		try
+		if (handler.numRecipes() == 0)
 		{
-			handler.loadCraftingRecipes(handler.getOverlayIdentifier());
-		} catch (Exception e) {
-			e.printStackTrace();
-			ChatUtils.addChatError(sender, "Error on loadCraftingRecipes: %s", e);
-			return;
+			try
+			{
+				handler.loadCraftingRecipes(handler.getOverlayIdentifier());
+			} catch (Exception e)
+			{
+				e.printStackTrace();
+				ChatUtils.addChatError(sender, "Error on loadCraftingRecipes: %s", e);
+				return;
+			}
+		} else {
+			LOGGER.info("Not loading Crafting Recipes for Handler because it already has %d recipes loaded", handler.numRecipes());
 		}
+		int numRecipes = handler.numRecipes();
 		ChatUtils.addChatMessage(sender, "Found IRecipeHandler: %s", handler.getClass().getName());
 		ChatUtils.addChatMessage(sender, "Recipe Name: %s", handler.getRecipeName());
-		ChatUtils.addChatMessage(sender, "Recipe Count: %d", handler.numRecipes());
+		ChatUtils.addChatMessage(sender, "Recipe Count: %d", numRecipes);
 
 		boolean log = false;
 		boolean some = false;
-		List<Integer> skip = Lists.newArrayList();
+
+		Set<Integer> skip = Sets.newHashSet();
 		int multiplier = 1;
 		for (String s: params.subList(1, params.size())) {
 			if (s.equals("some")) {
 				some = true;
 			} else if (s.equals("log")) {
 				log = true;
+			} else if (s.equals("fulllist")) {
+				fulllists = true;
+			} else if (s.equals("unlocal")) {
+				unlocal = true;
 			} else if (s.startsWith("skip=")) {
 				for (String n : Splitter.on(',').omitEmptyStrings().split(s.substring(5)))
 				{
@@ -98,8 +126,66 @@ public class CollectSubCommand implements ISubCommand
 				ChatUtils.addChatError(sender, "Unknown option '%s'", s);
 				return;
 			}
+			//TODO Options: Searching OreDict instead of Fake Items (Enable to use OD for groups and single-items separate)
 		}
+
+		for (int i = 0; i < numRecipes; i++) {
+			List<PositionedStack> ingredients = handler.getIngredientStacks(i);
+			PositionedStack outStack = handler.getResultStack(i);
+			if (some && i < 3) {
+				ChatUtils.addChatMessage(sender, listOfPositionedStacksToString(ingredients, outStack, skip));
+			}
+			if (log) {
+				LOGGER.info(listOfPositionedStacksToString(ingredients, outStack, skip));
+			}
+		}
+ 	}
+
+	void resetSettings() {
+		fulllists = false;
+		unlocal = false;
 	}
 
+	boolean unlocal = false;
+	boolean fulllists = false;
+	
+	private String itemToString(ItemStack itemStack) {
+		if (unlocal) {
+			return String.format("%s|%s", GameRegistry.findUniqueIdentifierFor(itemStack.getItem()).toString(), itemStack.getItemDamage());
+		}
+		return "'" + itemStack.getDisplayName() + "'";
+	}
+
+	private String listOfPositionedStacksToString(List<PositionedStack> stacks, PositionedStack outStack, Set<Integer> skipSlots) {
+		StringBuilder sb = new StringBuilder();
+		for (int slotNum = 0; slotNum < stacks.size(); slotNum++)
+		{
+			if (slotNum != 0) sb.append(" + ");
+			if (skipSlots.contains(slotNum)) sb.append("(");
+			if (stacks.get(slotNum).items.length > 1) sb.append("[");
+			Iterator<ItemStack> iterator = Arrays.asList(stacks.get(slotNum).items).iterator();
+			if (iterator.hasNext())
+			{
+				sb.append(itemToString(iterator.next()));
+				if (fulllists)
+				{
+					while (iterator.hasNext())
+					{
+						sb.append(", ").append(itemToString(iterator.next()));
+					}
+				}
+				else if (iterator.hasNext())
+				{
+					sb.append(", ..");
+				}
+			} else {
+				sb.append('-');
+			}
+			if (stacks.get(slotNum).items.length > 1) sb.append(']');
+			if (skipSlots.contains(slotNum)) sb.append(")");
+		}
+		sb.append(" = ").append(itemToString(outStack.item));
+		return sb.toString();
+	}
 
 }
