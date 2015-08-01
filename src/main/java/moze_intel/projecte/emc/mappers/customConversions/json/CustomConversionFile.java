a72f35ddc373234a0a9321acebcbbaa74910c9d6
MaPePeR <MaPePeR@users.noreply.github.com>
2015-08-01 18:05:06 +0200

    CustomConversionFile.write-method

--- src/main/java/moze_intel/projecte/emc/mappers/customConversions/json/CustomConversionFile.java
+++ src/main/java/moze_intel/projecte/emc/mappers/customConversions/json/CustomConversionFile.java
@@ -1,7 +1,12 @@
 package moze_intel.projecte.emc.mappers.customConversions.json;
 
 import com.google.common.collect.Maps;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -58,4 +63,13 @@ public class CustomConversionFile
 	public String comment;
 	public Map<String, ConversionGroup> groups = Maps.newHashMap();
 	public FixedValues values = new FixedValues();
+
+	public void write(File file) throws IOException
+	{
+		FileWriter fileWriter = new FileWriter(file);
+		GsonBuilder builder = new GsonBuilder();
+		Gson gson = builder.setPrettyPrinting().create();
+		gson.toJson(this, fileWriter);
+		fileWriter.close();
+	}
 }
