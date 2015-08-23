6c11639eab3600b8e84455b80d9b174798e0bc57
Dwargkiller <peter.hammerskog@gmail.com>
2015-08-23 18:23:45 +0200

    Remove hidden shapeless

--- src/main/java/moze_intel/projecte/integration/MineTweaker/HiddenShapeless.java
+++ src/main/java/moze_intel/projecte/integration/MineTweaker/HiddenShapeless.java
@@ -1,5 +1,79 @@
 package moze_intel.projecte.integration.MineTweaker;
 
+import minetweaker.IUndoableAction;
+import minetweaker.MineTweakerAPI;
+import moze_intel.projecte.gameObjs.customRecipes.RecipeShapelessHidden;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.CraftingManager;
+import net.minecraft.item.crafting.IRecipe;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import java.util.List;
+
+@ZenClass("mods.projecte.HiddenShapeless")
 public class HiddenShapeless
 {
+	@ZenMethod
+	public static void removeRecipe(ItemStack output)
+	{
+		MineTweakerAPI.apply(new Remove(output));
+	}
+
+	private static class Remove implements IUndoableAction
+	{
+		IRecipe recipe = null;
+		ItemStack remove;
+
+		public Remove(ItemStack rem){
+			remove = rem;
+		}
+
+		@Override
+		public void apply(){
+
+			List<IRecipe> recipes = CraftingManager.getInstance().getRecipeList();
+
+			for (IRecipe irecipe : recipes)
+			{
+				if (irecipe instanceof RecipeShapelessHidden)
+				{
+					if (irecipe.getRecipeOutput().isItemEqual(remove))
+					{
+						CraftingManager.getInstance().getRecipeList().remove(irecipe);
+					}
+				}
+			}
+		}
+
+		@Override
+		public boolean canUndo(){
+			return recipe != null;
+		}
+
+		@Override
+		public void undo(){
+			CraftingManager.getInstance().getRecipeList().add(recipe);
+		}
+
+		@Override
+		public String describe(){
+			return "Removing Hidden Crafting Recipe for " + recipe.getRecipeOutput().getDisplayName();
+		}
+
+		@Override
+		public String describeUndo(){
+			return "Un-removing Hidden Crafting Recipe for " + recipe.getRecipeOutput().getDisplayName();
+		}
+
+		@Override
+		public Object getOverrideKey() {
+			return null;
+		}
+
+	}
+
+
+
+
 }
