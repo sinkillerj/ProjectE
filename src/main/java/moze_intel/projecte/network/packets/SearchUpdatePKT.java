68143c4686124ad35d7e91b1dcf46ef30660b5a3
MaPePeR <MaPePeR@users.noreply.github.com>
2015-08-23 08:57:29 +0200

    Update single output slot when clicked on client

--- src/main/java/moze_intel/projecte/network/packets/SearchUpdatePKT.java
+++ src/main/java/moze_intel/projecte/network/packets/SearchUpdatePKT.java
@@ -1,6 +1,5 @@
 package moze_intel.projecte.network.packets;
 
-import com.google.common.collect.Lists;
 import cpw.mods.fml.common.network.ByteBufUtils;
 import cpw.mods.fml.common.network.simpleimpl.IMessage;
 import cpw.mods.fml.common.network.simpleimpl.IMessageHandler;
@@ -11,38 +10,31 @@ import net.minecraft.item.ItemStack;
 import moze_intel.projecte.gameObjs.container.TransmutationContainer;
 import moze_intel.projecte.utils.PELogger;
 
-import java.util.List;
 
 public class SearchUpdatePKT implements IMessage
 {
 	public SearchUpdatePKT() {}
 
-	public boolean applyImmediately = false;
-	public List<ItemStack> outslots;
-	public SearchUpdatePKT(List<ItemStack> outslots, boolean applyImmediately)
+	public int slot;
+	public ItemStack itemStack;
+	public SearchUpdatePKT(int slot, ItemStack itemStack)
 	{
-		this.applyImmediately = applyImmediately;
-		this.outslots = outslots;
+		this.slot = slot;
+		this.itemStack = itemStack.copy();
 	}
 
 	@Override
 	public void fromBytes(ByteBuf buf)
 	{
-		applyImmediately = buf.readBoolean();
-		List<ItemStack> l = Lists.newArrayList();
-		for (int i = 0; i < 16; i++) {
-			l.add(ByteBufUtils.readItemStack(buf));
-		}
-		this.outslots = l;
+		slot = buf.readInt();
+		itemStack = ByteBufUtils.readItemStack(buf);
 	}
 
 	@Override
 	public void toBytes(ByteBuf buf)
 	{
-		buf.writeBoolean(applyImmediately);
-		for (int i = 0; i < 16; i++) {
-			ByteBufUtils.writeItemStack(buf, outslots.get(i));
-		}
+		buf.writeInt(slot);
+		ByteBufUtils.writeItemStack(buf, itemStack);
 	}
 
 	public static class Handler implements IMessageHandler<SearchUpdatePKT, IMessage>
@@ -53,22 +45,8 @@ public class SearchUpdatePKT implements IMessage
 			if (ctx.getServerHandler().playerEntity.openContainer instanceof TransmutationContainer)
 			{
 				TransmutationContainer container = ((TransmutationContainer) ctx.getServerHandler().playerEntity.openContainer);
-				if (pkt.applyImmediately)
-				{
-					container.transmutationInventory.writeIntoOutputSlots(pkt.outslots);
-					PELogger.logFatal("Wrote Output Slots from UpdatePacket immediately");
-				}
-				else
-				{
-					try
-					{
-						container.transmutationInventory.serverOutputSlotUpdates.put(pkt.outslots);
-					} catch (InterruptedException e)
-					{
-						e.printStackTrace();
-					}
-					PELogger.logFatal("Got Output Slots from UpdatePacket... Size: " + container.transmutationInventory.serverOutputSlotUpdates.size());
-				}
+				container.transmutationInventory.writeIntoOutputSlot(pkt.slot, pkt.itemStack);
+				PELogger.logFatal("Wrote Output Slots from UpdatePacket immediately");
 			}
 
 			return null;
