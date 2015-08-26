c8701e4a13b1a3009aaa040cabf28e507b3d40c0
Dwargkiller <peter.hammerskog@gmail.com>
2015-08-26 18:09:27 +0200

    World transmutes in minetweaker

--- src/main/java/moze_intel/projecte/integration/MineTweaker/PhiloStone.java
+++ src/main/java/moze_intel/projecte/integration/MineTweaker/PhiloStone.java
@@ -7,6 +7,9 @@ import minetweaker.api.item.IItemStack;
 import minetweaker.api.minecraft.MineTweakerMC;
 import moze_intel.projecte.gameObjs.ObjHandler;
 import moze_intel.projecte.gameObjs.customRecipes.RecipeShapelessHidden;
+import moze_intel.projecte.utils.MetaBlock;
+import moze_intel.projecte.utils.PELogger;
+import moze_intel.projecte.utils.WorldTransmutations;
 import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.CraftingManager;
@@ -15,6 +18,7 @@ import net.minecraftforge.oredict.OreDictionary;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
@@ -37,6 +41,30 @@ public class PhiloStone
 	public static void removePhiloSmelting(IItemStack output)
 	{
 		MineTweakerAPI.apply(new RemoveRecipeAction(output));
+	}
+
+	@ZenMethod
+	public static void addWorldTransmutation(IItemStack output, IItemStack input)
+	{
+		MineTweakerAPI.apply(new AddWorldTransmutationAction(output, input));
+	}
+
+	@ZenMethod
+	public static void addWorldTransmutation(IItemStack output, IItemStack sneakOutput, IItemStack input)
+	{
+		MineTweakerAPI.apply(new AddWorldTransmutationAction(output, sneakOutput, input));
+	}
+
+	@ZenMethod
+	public static void removeWorldTransmutation(IItemStack output, IItemStack input)
+	{
+		MineTweakerAPI.apply(new RemoveWorldTransmutationAction(output, input));
+	}
+
+	@ZenMethod
+	public static void removeWorldTransmutation(IItemStack output, IItemStack sneakOutput, IItemStack input)
+	{
+		MineTweakerAPI.apply(new RemoveWorldTransmutationAction(output, sneakOutput, input));
 	}
 
 
@@ -200,4 +228,176 @@ public class PhiloStone
 
 	}
 
+	private static class AddWorldTransmutationAction implements IUndoableAction
+	{
+		private final MetaBlock output;
+		private final MetaBlock sneakOutput;
+		private final MetaBlock input;
+
+		public AddWorldTransmutationAction(IItemStack output, IItemStack input)
+		{
+			this.output = new MetaBlock(MineTweakerMC.getBlock(output));
+			this.sneakOutput = null;
+			this.input = new MetaBlock(MineTweakerMC.getBlock(input));
+
+		}
+
+		public AddWorldTransmutationAction(IItemStack output, IItemStack sneakOutput, IItemStack input)
+		{
+			this.output = new MetaBlock(MineTweakerMC.getBlock(output));
+			this.sneakOutput = new MetaBlock(MineTweakerMC.getBlock(sneakOutput));
+			this.input = new MetaBlock(MineTweakerMC.getBlock(input));
+		}
+
+		@Override
+		public void apply()
+		{
+			WorldTransmutations.register(this.input, this.output, this.sneakOutput);
+		}
+
+		@Override
+		public boolean canUndo()
+		{
+			return true;
+		}
+
+		@Override
+		public void undo()
+		{
+			Iterator<Map.Entry<MetaBlock, MetaBlock[]>> it = WorldTransmutations.MAP.entrySet().iterator();
+
+			while (it.hasNext())
+			{
+				Map.Entry<MetaBlock, MetaBlock[]> entry = it.next();
+
+				if (entry.getKey().equals(this.input) && entry.getValue()[0].equals(this.output))
+				{
+					if (entry.getValue().length > 1 && !entry.getValue()[1].equals(this.sneakOutput))
+					{
+						PELogger.logInfo("First check failed");
+						continue;
+					} else
+					{
+						PELogger.logInfo("Removal Succeeded");
+						it.remove();
+					}
+				}
+
+			}
+		}
+
+		@Override
+		public String describe()
+		{
+			return "Adding world transmutation recipe for " + output;
+		}
+
+		@Override
+		public String describeUndo()
+		{
+			return "Removing world transmutation recipe for " + output;
+		}
+
+		@Override
+		public Object getOverrideKey()
+		{
+			return null;
+		}
+	}
+
+	private static class RemoveWorldTransmutationAction implements IUndoableAction
+	{
+
+		private final MetaBlock output;
+		private final MetaBlock sneakOutput;
+		private final MetaBlock input;
+
+		public RemoveWorldTransmutationAction(IItemStack output, IItemStack input)
+		{
+			this.output = new MetaBlock(MineTweakerMC.getBlock(output));
+			this.sneakOutput = null;
+			this.input = new MetaBlock(MineTweakerMC.getBlock(input));
+		}
+
+
+		public RemoveWorldTransmutationAction(IItemStack output, IItemStack sneakOutput, IItemStack input)
+		{
+			this.output = new MetaBlock(MineTweakerMC.getBlock(output));
+			this.sneakOutput = new MetaBlock(MineTweakerMC.getBlock(sneakOutput));
+			this.input = new MetaBlock(MineTweakerMC.getBlock(input));
+		}
+
+		@Override
+		public void apply()
+		{
+
+			Iterator<Map.Entry<MetaBlock, MetaBlock[]>> it = WorldTransmutations.MAP.entrySet().iterator();
+
+			while (it.hasNext())
+			{
+				Map.Entry<MetaBlock, MetaBlock[]> entry = it.next();
+
+				if (entry.getKey().equals(this.input) && entry.getValue()[0].equals(this.output))
+				{
+					if (entry.getValue().length > 1 && !entry.getValue()[1].equals(this.sneakOutput))
+					{
+						PELogger.logInfo("First check failed");
+						continue;
+					} else
+					{
+						PELogger.logInfo("Removal Succeeded");
+						it.remove();
+					}
+				}
+
+			}
+			
+			/*for (Map.Entry<MetaBlock, MetaBlock[]> entry : WorldTransmutations.MAP.entrySet())
+			{
+
+				if (entry.getKey().equals(this.input) && entry.getValue()[0].equals(this.output))
+				{
+					if (entry.getValue().length > 1 && !entry.getValue()[1].equals(this.sneakOutput))
+					{
+						PELogger.logInfo("First check failed");
+						continue;
+					} else
+					{
+						PELogger.logInfo("Removal Succeeded");
+						WorldTransmutations.MAP.remove(entry);
+					}
+				}
+			}*/
+		}
+
+		@Override
+		public boolean canUndo()
+		{
+			return true;
+		}
+
+		@Override
+		public void undo()
+		{
+			WorldTransmutations.register(this.input, this.output, this.sneakOutput);
+		}
+
+		@Override
+		public String describe()
+		{
+			return "Removing world transmutation recipe for " + output;
+		}
+
+		@Override
+		public String describeUndo()
+		{
+			return "Un-removing world transmutation recipe for " + output;
+		}
+
+		@Override
+		public Object getOverrideKey()
+		{
+			return null;
+		}
+	}
 }
