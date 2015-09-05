17cacb014be350fb490b417c9deef5712bb2108d
MaPePeR <MaPePeR@users.noreply.github.com>
2015-09-06 00:15:15 +0200

    Fix oversight when solving merge conflicts

--- src/main/java/moze_intel/projecte/emc/collector/DumpToFileCollector.java
+++ src/main/java/moze_intel/projecte/emc/collector/DumpToFileCollector.java
@@ -34,7 +34,7 @@ public class DumpToFileCollector<A extends IValueArithmetic> extends AbstractMap
 	@Override
 	public void addConversion(int outnumber, NormalizedSimpleStack output, Map<NormalizedSimpleStack, Integer> ingredientsWithAmount, A arithmeticForConversion)
 	{
-		inner.addConversion(outnumber, output, ingredientsWithAmount);
+		inner.addConversion(outnumber, output, ingredientsWithAmount, arithmeticForConversion);
 		if (output == null || ingredientsWithAmount.containsKey(null)) return;
 		if (!out.groups.containsKey(currentGroupName)) out.groups.put(currentGroupName, new ConversionGroup());
 		ConversionGroup group = out.groups.get(currentGroupName);
