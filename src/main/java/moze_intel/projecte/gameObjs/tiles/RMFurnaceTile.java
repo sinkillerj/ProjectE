59f1562245a58d4a515e778bf2d19b27446e8f3a
MaPePeR <MaPePeR@users.noreply.github.com>
2015-09-13 22:03:06 +0200

    Make Matter Furnaces Accept EMC from adjacent emc providers

--- src/main/java/moze_intel/projecte/gameObjs/tiles/RMFurnaceTile.java
+++ src/main/java/moze_intel/projecte/gameObjs/tiles/RMFurnaceTile.java
@@ -3,6 +3,7 @@ package moze_intel.projecte.gameObjs.tiles;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import moze_intel.projecte.api.item.IItemEmc;
+import moze_intel.projecte.api.tile.IEmcAcceptor;
 import moze_intel.projecte.gameObjs.blocks.MatterFurnace;
 import moze_intel.projecte.utils.ItemHelper;
 import net.minecraft.block.Block;
@@ -18,7 +19,7 @@ import net.minecraft.tileentity.TileEntityFurnace;
 import net.minecraft.util.Facing;
 import net.minecraftforge.common.util.ForgeDirection;
 
-public class RMFurnaceTile extends TileEmc implements IInventory, ISidedInventory
+public class RMFurnaceTile extends TileEmc implements IInventory, ISidedInventory, IEmcAcceptor
 {
 	private final float EMC_CONSUMPTION = 1.6f;
 	public ItemStack[] inventory = new ItemStack[27];
@@ -731,5 +732,18 @@ public class RMFurnaceTile extends TileEmc implements IInventory, ISidedInventor
 	public boolean canExtractItem(int slot, ItemStack stack, int side) 
 	{
 		return slot >= outputStorage[0];
+	}
+
+	@Override
+	public double acceptEMC(ForgeDirection side, double toAccept)
+	{
+		if (this.getStoredEmc() < EMC_CONSUMPTION)
+		{
+			double needed = EMC_CONSUMPTION - this.getStoredEmc();
+			double accept = Math.min(needed, toAccept);
+			this.addEMC(accept);
+			return accept;
+		}
+		return 0;
 	}
 }
