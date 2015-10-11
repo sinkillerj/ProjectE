7be72fa35af5af3e992818e33bf7759f1496af04
sinkillerj <sinkillerj@users.noreply.github.com>
2015-10-11 17:05:16 -0400

    Fix duplicate tooltips

--- src/main/java/moze_intel/projecte/PECore.java
+++ src/main/java/moze_intel/projecte/PECore.java
@@ -95,8 +95,6 @@ public class PECore
 		FMLCommonHandler.instance().bus().register(new TickEvents());
 		FMLCommonHandler.instance().bus().register(new ConnectionHandler());
 
-		proxy.registerClientOnlyEvents();
-
 		ObjHandler.register();
 		ObjHandler.addRecipes();
 
