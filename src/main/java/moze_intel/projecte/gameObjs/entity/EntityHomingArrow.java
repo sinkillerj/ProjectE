5b94377652d09c5456a16a481a43be7b550ab5ac
Vincent Lee <vincent.lee@outlook.com>
2015-12-18 17:54:11 -0600

    Fix up what's already there

--- src/main/java/moze_intel/projecte/gameObjs/entity/EntityHomingArrow.java
+++ src/main/java/moze_intel/projecte/gameObjs/entity/EntityHomingArrow.java
@@ -1,105 +1,118 @@
 package moze_intel.projecte.gameObjs.entity;
 
+import moze_intel.projecte.utils.PELogger;
 import moze_intel.projecte.utils.WorldHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLiving;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.world.World;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class EntityHomingArrow extends EntityArrow
 {
-	EntityLiving target;
-	World world;
+	private static final int DW_TARGET_ID = 31;
+	private static final int NO_TARGET = -1;
 
-	private void init(World world)
-	{
-		this.world = world;
-	}
+	private int newTargetCooldown = 0;
 
 	public EntityHomingArrow(World world)
 	{
 		super(world);
-		init(world);
 	}
 
 	public EntityHomingArrow(World world, EntityLivingBase par2, float par3) 
 	{
 		super(world, par2, par3);
-		init(world);
 	}
-	
+
+	@Override
+	public void entityInit()
+	{
+		super.entityInit();
+		setIsCritical(true);
+		dataWatcher.addObject(DW_TARGET_ID, NO_TARGET); // Target entity id
+	}
+
 	@Override
 	public void onUpdate()
 	{
-		//TODO Create proper custom arrow. This one is duplicating because of the super call for onUpdate();
-		super.onUpdate();
+		if (!worldObj.isRemote)
+		{
+			if (hasTarget() && !getTarget().isEntityAlive())
+			{
+				dataWatcher.updateObject(DW_TARGET_ID, NO_TARGET);
+				PELogger.logInfo("Removing dead target");
+			}
+
+			if (!hasTarget() && !WorldHelper.isArrowInGround(this) && newTargetCooldown <= 0)
+			{
+				PELogger.logInfo("Finding new target");
+				findNewTarget();
+			} else
+			{
+				newTargetCooldown--;
+			}
+		}
+
+		if (!hasTarget())
+		{
+			super.onUpdate();
+			return;
+		}
 
 		AxisAlignedBB box = this.boundingBox;
 		
-		if (target == null && !WorldHelper.isArrowInGround(this))
+		if (!WorldHelper.isArrowInGround(this))
 		{
-			AxisAlignedBB bBox = box.expand(8, 8, 8);
-			List<EntityLiving> list = this.worldObj.getEntitiesWithinAABB(EntityLiving.class, bBox);
-
-			double distance = 100000;
-
-			for (EntityLiving entity : list)
-			{
-				double toIt = distanceTo(entity);
-
-				if (distance > toIt)
-				{
-					distance = toIt;
-					target = entity;
-				}
-			}
-			
-			if (target == null)
-			{
-				return;
-			}
-
+			worldObj.spawnParticle("flame", box.maxX, box.maxY, box.maxZ, 0.0D, 0.0D, 0.0D);
+			Entity target = getTarget();
 			double d5 = target.posX - this.posX;
 			double d6 = target.boundingBox.minY + target.height - this.posY;
 			double d7 = target.posZ - this.posZ;
 			
-			this.setThrowableHeading(d5, d6, d7, 2.0F, 0.0F);
-		}
-		else if (!WorldHelper.isArrowInGround(this))
-		{
-			if (target.getHealth() == 0) 
-			{
-				target = null;
-				return;
-			}
-
-			world.spawnParticle("flame", box.maxX, box.maxY, box.maxZ, 0.0D, 0.0D, 0.0D);
-
-			double d5 = target.posX - this.posX;
-			double d6 = target.boundingBox.minY + target.height - this.posY;
-			double d7 = target.posZ - this.posZ;
-			
-			this.setThrowableHeading(d5, d6, d7, 2.0F, 0.0F);
+			this.setThrowableHeading(d5, d6, d7, 0.1F, 0.0F);
+			super.onUpdate();
 		}
 	}
 
-	private double distanceTo(EntityLiving entity)
+	private void findNewTarget()
 	{
-		double [] ds = new double []
+		List<EntityLiving> candidates = worldObj.getEntitiesWithinAABB(EntityLiving.class, this.boundingBox.expand(8, 8, 8));
+		Collections.sort(candidates, new Comparator<EntityLiving>() {
+			@Override
+			public int compare(EntityLiving o1, EntityLiving o2) {
+				double dist = EntityHomingArrow.this.getDistanceSqToEntity(o1) - EntityHomingArrow.this.getDistanceSqToEntity(o2);
+				if (dist == 0.0)
+				{
+					return 0;
+				} else
+				{
+					return dist > 0.0 ? 1 : -1;
+				}
+			}
+		});
+
+		if (!candidates.isEmpty())
 		{
-			this.posX - entity.posX,
-			this.posY - entity.posY,
-			this.posZ - entity.posZ
-		};
+			dataWatcher.updateObject(DW_TARGET_ID, candidates.get(0).getEntityId());
+			PELogger.logInfo("Found new target");
+		}
 
-		double d = 0;
+		newTargetCooldown = 5;
+	}
 
-		for (int i = 0; i < 3; i++)
-			d += ds[i] * ds[i];
+	private EntityLiving getTarget()
+	{
+		return ((EntityLiving) worldObj.getEntityByID(dataWatcher.getWatchableObjectInt(DW_TARGET_ID)));
+	}
 
-		return Math.sqrt(d);
+	private boolean hasTarget()
+	{
+		return getTarget() != null;
 	}
 }
