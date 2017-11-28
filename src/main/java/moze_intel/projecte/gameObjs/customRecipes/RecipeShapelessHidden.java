324772087ecbc7668ac5aea7a2f7ded122ebabc3
Vincent Lee <vincent.lee@outlook.com>
2017-11-27 21:48:35 -0600

    Properly hide philstone smelting recipes from JEI, without vanillacopying

--- src/main/java/moze_intel/projecte/gameObjs/customRecipes/RecipeShapelessHidden.java
+++ src/main/java/moze_intel/projecte/gameObjs/customRecipes/RecipeShapelessHidden.java
@@ -1,19 +1,72 @@
 package moze_intel.projecte.gameObjs.customRecipes;
 
+import net.minecraft.inventory.InventoryCrafting;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.crafting.IRecipe;
 import net.minecraft.item.crafting.Ingredient;
 import net.minecraft.item.crafting.ShapelessRecipes;
 import net.minecraft.util.NonNullList;
+import net.minecraft.world.World;
 
-public class RecipeShapelessHidden extends ShapelessRecipes
+import javax.annotation.Nonnull;
+
+/**
+ * Composes a ShapelessRecipes to hide it from JEI
+ */
+public class RecipeShapelessHidden extends net.minecraftforge.registries.IForgeRegistryEntry.Impl<IRecipe> implements IRecipe
 {
+	private final ShapelessRecipes compose;
+
 	public RecipeShapelessHidden(String group, ItemStack result, NonNullList<Ingredient> ingredients) {
-		super(group, result, ingredients);
+		this.compose = new ShapelessRecipes(group, result, ingredients);
 	}
 
-	// TODO 1.12 - ensure JEI respects this or bother them to do so
 	@Override
-	public boolean isHidden() {
+	public boolean matches(@Nonnull InventoryCrafting inv, @Nonnull World worldIn) {
+		return compose.matches(inv, worldIn);
+	}
+
+	@Nonnull
+	@Override
+	public ItemStack getCraftingResult(@Nonnull InventoryCrafting inv) {
+		return compose.getCraftingResult(inv);
+	}
+
+	@Override
+	public boolean canFit(int width, int height) {
+		return compose.canFit(width, height);
+	}
+
+	@Nonnull
+	@Override
+	public ItemStack getRecipeOutput() {
+		return compose.getRecipeOutput();
+	}
+
+	@Nonnull
+	@Override
+	public NonNullList<ItemStack> getRemainingItems(InventoryCrafting inv)
+	{
+		return compose.getRemainingItems(inv);
+	}
+
+	@Nonnull
+	@Override
+	public NonNullList<Ingredient> getIngredients()
+	{
+		return compose.getIngredients();
+	}
+
+	@Override
+	public boolean isHidden()
+	{
 		return true;
+	}
+
+	@Nonnull
+	@Override
+	public String getGroup()
+	{
+		return compose.getGroup();
 	}
 }
