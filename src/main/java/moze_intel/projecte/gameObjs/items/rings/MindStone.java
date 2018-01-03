2e511e7c0cdd8447ec205a4f39d0adb1164d4010
Vincent Lee <vincent.lee@outlook.com>
2018-01-03 16:20:19 -0600

    Fix mind stone needing to be clicked on a block to extract

--- src/main/java/moze_intel/projecte/gameObjs/items/rings/MindStone.java
+++ src/main/java/moze_intel/projecte/gameObjs/items/rings/MindStone.java
@@ -13,6 +13,7 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -60,7 +61,7 @@ public class MindStone extends RingToggle implements IPedestalItem
 	
 	@Nonnull
 	@Override
-	public EnumActionResult onItemUse(EntityPlayer player, World world, BlockPos pos, EnumHand hand, EnumFacing side, float par8, float par9, float par10)
+	public ActionResult<ItemStack> onItemRightClick(World world, EntityPlayer player, EnumHand hand)
 	{
 		ItemStack stack = player.getHeldItem(hand);
 		if (!world.isRemote && stack.getItemDamage() == 0 && getStoredXP(stack) != 0)
@@ -73,7 +74,7 @@ public class MindStone extends RingToggle implements IPedestalItem
 			}
 		}
 		
-		return EnumActionResult.SUCCESS;
+		return ActionResult.newResult(EnumActionResult.SUCCESS, stack);
 	}
 
 	@SideOnly(Side.CLIENT)
