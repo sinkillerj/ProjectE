739c406d303ef63b2ab2d74582d7b73583b19d46
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2018-03-30 17:21:10 -0400

    Return early if tile is null

--- src/main/java/moze_intel/projecte/gameObjs/items/rings/BodyStone.java
+++ src/main/java/moze_intel/projecte/gameObjs/items/rings/BodyStone.java
@@ -134,7 +134,11 @@ public class BodyStone extends RingToggle implements IBauble, IPedestalItem
 		if (!world.isRemote && ProjectEConfig.bodyPedCooldown != -1)
 		{
 			DMPedestalTile tile = ((DMPedestalTile) world.getTileEntity(pos));
-			if (tile != null && tile.getActivityCooldown() == 0)
+			if(tile == null)
+			{
+				return;
+			}
+			if (tile.getActivityCooldown() == 0)
 			{
 				List<EntityPlayerMP> players = world.getEntitiesWithinAABB(EntityPlayerMP.class, tile.getEffectBounds());
 
@@ -151,10 +155,7 @@ public class BodyStone extends RingToggle implements IBauble, IPedestalItem
 			}
 			else
 			{
-				if(tile != null)
-				{
-					tile.decrementActivityCooldown();
-				}
+				tile.decrementActivityCooldown();
 			}
 		}
 	}
