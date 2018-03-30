46172a482e4f07749e4c327ff23c4dc6b7c0656c
Benjamin Urquhart <24701883+BenjaminUrquhart@users.noreply.github.com>
2018-03-30 17:19:43 -0400

    Return early if tile is null

--- src/main/java/moze_intel/projecte/gameObjs/items/rings/Ignition.java
+++ src/main/java/moze_intel/projecte/gameObjs/items/rings/Ignition.java
@@ -157,7 +157,11 @@ public class Ignition extends RingToggle implements IBauble, IPedestalItem, IFir
 		if (!world.isRemote && ProjectEConfig.ignitePedCooldown != -1)
 		{
 			DMPedestalTile tile = ((DMPedestalTile) world.getTileEntity(pos));
-			if (tile != null && tile.getActivityCooldown() == 0)
+			if(tile == null)
+			{
+				return;
+			}
+			if (tile.getActivityCooldown() == 0)
 			{
 				List<EntityLiving> list = world.getEntitiesWithinAABB(EntityLiving.class, tile.getEffectBounds());
 				for (EntityLiving living : list)
@@ -170,10 +174,7 @@ public class Ignition extends RingToggle implements IBauble, IPedestalItem, IFir
 			}
 			else
 			{
-				if(tile != null)
-				{
-					tile.decrementActivityCooldown();
-				}
+				tile.decrementActivityCooldown();
 			}
 		}
 	}
