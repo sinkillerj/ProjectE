93e98bb7e915fe38c9852591b0d4a5879532ba7d
pupnewfster <richard@freimer.com>
2018-06-28 01:06:13 -0400

    CraftTweaker Recipe Mapping (#1714)
    * Closes #1713
    
    * Addresses comments from pull review

---
+++ src/main/java/moze_intel/projecte/emc/mappers/CraftTweakerRecipeMapper.java
@@ -0,0 +1,31 @@
+package moze_intel.projecte.emc.mappers;
+
+import crafttweaker.mc1120.recipes.MCRecipeShaped;
+import crafttweaker.mc1120.recipes.MCRecipeShapeless;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraftforge.fml.common.Loader;
+import net.minecraftforge.fml.common.versioning.VersionParser;
+
+public class CraftTweakerRecipeMapper implements CraftingMapper.IRecipeMapper {
+    private boolean ctCompat;
+
+    public CraftTweakerRecipeMapper() {
+        //Check to make sure it is a version of CraftTweaker that uses the new Recipe System
+        ctCompat = Loader.isModLoaded("crafttweaker") && VersionParser.parseVersionReference("crafttweaker@[4.1.5,)").containsVersion(Loader.instance().getIndexedModList().get("crafttweaker").getProcessedVersion());
+    }
+
+    @Override
+    public String getName() {
+        return "CraftTweakerRecipeMapper";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Maps `IRecipe` CraftTweaker crafting recipes that extend `MCRecipeShaped` or `MCRecipeShapeless";
+    }
+
+    @Override
+    public boolean canHandle(IRecipe recipe) {
+        return ctCompat && (recipe instanceof MCRecipeShaped || recipe instanceof MCRecipeShapeless);
+    }
+}
