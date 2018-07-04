757de6cc4dc32154c87d95da7c2b8497923ea391
Vincent Lee <williewillus@users.noreply.github.com>
2018-07-03 20:19:35 -0700

    bump api version

--- src/main/java/moze_intel/projecte/api/package-info.java
+++ src/main/java/moze_intel/projecte/api/package-info.java
@@ -2,7 +2,7 @@
  * Increment apiVersion every time the API changes.
  * (Adding methods, removing methods, changing method signatures, etc.)
  */
-@API(owner = "projecte", apiVersion = "1.12.2-1.0.0", provides = "projecteapi")
+@API(owner = "projecte", apiVersion = "1.12.2-1.1.0", provides = "projecteapi")
 package moze_intel.projecte.api;
 
 import net.minecraftforge.fml.common.API;
