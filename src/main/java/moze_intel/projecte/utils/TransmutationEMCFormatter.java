24aebc9e52cd9741ddc49ffb5b8345fda83d9b57
James <jamestompkins@hotmail.co.uk>
2018-12-02 13:17:28 -0600

    Create TransmutationEMCFormatter.java
    Used to format EMC text within transmutation table

---
+++ src/main/java/moze_intel/projecte/utils/TransmutationEMCFormatter.java
@@ -0,0 +1,22 @@
+package moze_intel.projecte.utils;
+
+public class TransmutationEMCFormatter {
+	public static String EMCFormat(double EMC) {
+		String postFix = "";
+		double magnitude = 1;
+
+		String formats[] = {"Q", "T","B"};
+		Double magnitudes[] = {1e15, 1e12, 1e9};
+
+		for (int i=0; i <formats.length; i++) {
+			double testMag = magnitudes[i];
+			if (EMC >= testMag) {
+				magnitude = testMag;
+				postFix = formats[i];
+				break;
+			}
+		}
+
+		return Constants.EMC_FORMATTER.format(EMC / magnitude) + postFix;
+	}
+}
