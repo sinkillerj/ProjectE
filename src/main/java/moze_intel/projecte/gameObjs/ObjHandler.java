17fd45c11b802a8330f8dee88f56511b75b8d71d
Vincent Lee <vincent.lee@outlook.com>
2019-01-15 00:48:20 -0600

    Give new things emc values, fix mobius fuel recipe

--- src/main/java/moze_intel/projecte/gameObjs/ObjHandler.java
+++ src/main/java/moze_intel/projecte/gameObjs/ObjHandler.java
@@ -473,7 +473,6 @@ public class ObjHandler
 	@SubscribeEvent
 	public static void registerTileEntities(RegistryEvent.Register<TileEntityType<?>> evt)
 	{
-		System.out.println("Registering TE's");
 		evt.getRegistry().register(ALCH_CHEST_TILE);
 		evt.getRegistry().register(INTERDICTION_TORCH_TILE);
 		evt.getRegistry().register(CONDENSER_TILE);
