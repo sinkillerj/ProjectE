219786b725372ab6e8a92ea5dfdf27ebab915cfc
pupnewfster <richard@freimer.com>
2019-01-29 13:26:00 -0500

    Replace spaces with tabs

--- src/main/java/moze_intel/projecte/integration/crafttweaker/EntityRandomizer.java
+++ src/main/java/moze_intel/projecte/integration/crafttweaker/EntityRandomizer.java
@@ -13,72 +13,72 @@ import stanhebben.zenscript.annotations.ZenMethod;
 @ZenClass("mods.projecte.EntityRandomizer")
 public class EntityRandomizer
 {
-    @ZenMethod
-    public static void addPeaceful(IEntityDefinition entityDefinition)
-    {
-        Class<? extends EntityLiving> living = getLiving(entityDefinition);
-        if (isLiving(living)) {
-            CraftTweakerAPI.apply(new EntityRandomizerAction.Add(living, entityDefinition.getName(), true));
-        }
-    }
+	@ZenMethod
+	public static void addPeaceful(IEntityDefinition entityDefinition)
+	{
+		Class<? extends EntityLiving> living = getLiving(entityDefinition);
+		if (isLiving(living)) {
+			CraftTweakerAPI.apply(new EntityRandomizerAction.Add(living, entityDefinition.getName(), true));
+		}
+	}
 
-    @ZenMethod
-    public static void removePeaceful(IEntityDefinition entityDefinition)
-    {
-        Class<? extends EntityLiving> living = getLiving(entityDefinition);
-        if (isLiving(living)) {
-            CraftTweakerAPI.apply(new EntityRandomizerAction.Remove(living, entityDefinition.getName(), true));
-        }
-    }
+	@ZenMethod
+	public static void removePeaceful(IEntityDefinition entityDefinition)
+	{
+		Class<? extends EntityLiving> living = getLiving(entityDefinition);
+		if (isLiving(living)) {
+			CraftTweakerAPI.apply(new EntityRandomizerAction.Remove(living, entityDefinition.getName(), true));
+		}
+	}
 
-    @ZenMethod
-    public static void clearPeacefuls()
-    {
-        CraftTweakerAPI.apply(new EntityRandomizerAction.Clear(true));
-    }
+	@ZenMethod
+	public static void clearPeacefuls()
+	{
+		CraftTweakerAPI.apply(new EntityRandomizerAction.Clear(true));
+	}
 
-    @ZenMethod
-    public static void addMob(IEntityDefinition entityDefinition)
-    {
-        Class<? extends EntityLiving> living = getLiving(entityDefinition);
-        if (isLiving(living)) {
-            CraftTweakerAPI.apply(new EntityRandomizerAction.Add(living, entityDefinition.getName(), false));
-        }
-    }
+	@ZenMethod
+	public static void addMob(IEntityDefinition entityDefinition)
+	{
+		Class<? extends EntityLiving> living = getLiving(entityDefinition);
+		if (isLiving(living)) {
+			CraftTweakerAPI.apply(new EntityRandomizerAction.Add(living, entityDefinition.getName(), false));
+		}
+	}
 
-    @ZenMethod
-    public static void removeMob(IEntityDefinition entityDefinition)
-    {
-        Class<? extends EntityLiving> living = getLiving(entityDefinition);
-        if (isLiving(living)) {
-            CraftTweakerAPI.apply(new EntityRandomizerAction.Remove(living, entityDefinition.getName(), false));
-        }
-    }
+	@ZenMethod
+	public static void removeMob(IEntityDefinition entityDefinition)
+	{
+		Class<? extends EntityLiving> living = getLiving(entityDefinition);
+		if (isLiving(living)) {
+			CraftTweakerAPI.apply(new EntityRandomizerAction.Remove(living, entityDefinition.getName(), false));
+		}
+	}
 
-    @ZenMethod
-    public static void clearMobs()
-    {
-        CraftTweakerAPI.apply(new EntityRandomizerAction.Clear(false));
-    }
+	@ZenMethod
+	public static void clearMobs()
+	{
+		CraftTweakerAPI.apply(new EntityRandomizerAction.Clear(false));
+	}
 
-    private static Class<? extends EntityLiving> getLiving(IEntityDefinition entityDefinition) {
-        if (entityDefinition == null) {
-            return null;
-        }
-        EntityEntry entry = (EntityEntry) entityDefinition.getInternal();
-        Class<? extends Entity> entityClass = entry.getEntityClass();
-        if (EntityLiving.class.isAssignableFrom(entityClass)) {
-            return (Class<? extends EntityLiving>) entityClass;
-        }
-        return null;
-    }
+	private static Class<? extends EntityLiving> getLiving(IEntityDefinition entityDefinition) {
+		if (entityDefinition == null) {
+			return null;
+		}
+		EntityEntry entry = (EntityEntry) entityDefinition.getInternal();
+		Class<? extends Entity> entityClass = entry.getEntityClass();
+		if (EntityLiving.class.isAssignableFrom(entityClass)) {
+			return (Class<? extends EntityLiving>) entityClass;
+		}
+		return null;
+	}
 
-    private static boolean isLiving(Class<? extends EntityLiving> living) {
-        if (living == null)
-        {
-            CraftTweakerAPI.logError("IEntityDefinition must be of a living entity.");
-            return false;
-        }
-        return true;
-    }
+	private static boolean isLiving(Class<? extends EntityLiving> living) {
+		if (living == null)
+		{
+			CraftTweakerAPI.logError("IEntityDefinition must be of a living entity.");
+			return false;
+		}
+		return true;
+	}
 }
--- src/main/java/moze_intel/projecte/integration/crafttweaker/EntityRandomizerAction.java
+++ src/main/java/moze_intel/projecte/integration/crafttweaker/EntityRandomizerAction.java
@@ -5,79 +5,79 @@ import moze_intel.projecte.utils.WorldHelper;
 import net.minecraft.entity.EntityLiving;
 
 abstract class EntityRandomizerAction implements IAction {
-    final boolean peaceful;
+	final boolean peaceful;
 
-    EntityRandomizerAction(boolean peaceful) {
-        this.peaceful = peaceful;
-    }
+	EntityRandomizerAction(boolean peaceful) {
+		this.peaceful = peaceful;
+	}
 
-    static class Add extends EntityRandomizerAction {
-        private final Class<? extends EntityLiving> living;
-        private final String typeName;
+	static class Add extends EntityRandomizerAction {
+		private final Class<? extends EntityLiving> living;
+		private final String typeName;
 
-        Add(Class<? extends EntityLiving> living, String typeName, boolean peaceful) {
-            super(peaceful);
-            this.living = living;
-            this.typeName = typeName;
-        }
+		Add(Class<? extends EntityLiving> living, String typeName, boolean peaceful) {
+			super(peaceful);
+			this.living = living;
+			this.typeName = typeName;
+		}
 
-        @Override
-        public void apply() {
-            if (this.peaceful) {
-                WorldHelper.addPeaceful(living);
-            } else {
-                WorldHelper.addMob(living);
-            }
-        }
+		@Override
+		public void apply() {
+			if (this.peaceful) {
+				WorldHelper.addPeaceful(living);
+			} else {
+				WorldHelper.addMob(living);
+			}
+		}
 
-        @Override
-        public String describe() {
-            return "Added " + this.typeName + " to the " + (this.peaceful ? "peaceful" : "hostile") + " Philosopher Stone Entity Randomizer.";
-        }
-    }
+		@Override
+		public String describe() {
+			return "Added " + this.typeName + " to the " + (this.peaceful ? "peaceful" : "hostile") + " Philosopher Stone Entity Randomizer.";
+		}
+	}
 
-    static class Remove extends EntityRandomizerAction {
-        private final Class<? extends EntityLiving> living;
-        private final String typeName;
+	static class Remove extends EntityRandomizerAction {
+		private final Class<? extends EntityLiving> living;
+		private final String typeName;
 
-        Remove(Class<? extends EntityLiving> living, String typeName, boolean peaceful) {
-            super(peaceful);
-            this.living = living;
-            this.typeName = typeName;
-        }
+		Remove(Class<? extends EntityLiving> living, String typeName, boolean peaceful) {
+			super(peaceful);
+			this.living = living;
+			this.typeName = typeName;
+		}
 
-        @Override
-        public void apply() {
-            if (this.peaceful) {
-                WorldHelper.removePeaceful(this.living);
-            } else {
-                WorldHelper.removeMob(this.living);
-            }
-        }
+		@Override
+		public void apply() {
+			if (this.peaceful) {
+				WorldHelper.removePeaceful(this.living);
+			} else {
+				WorldHelper.removeMob(this.living);
+			}
+		}
 
-        @Override
-        public String describe() {
-            return "Removed " + this.typeName + " from the " + (this.peaceful ? "peaceful" : "hostile") + " Philosopher Stone Entity Randomizer.";
-        }
-    }
+		@Override
+		public String describe() {
+			return "Removed " + this.typeName + " from the " + (this.peaceful ? "peaceful" : "hostile") + " Philosopher Stone Entity Randomizer.";
+		}
+	}
 
-    static class Clear extends EntityRandomizerAction {
-        Clear(boolean peaceful) {
-            super(peaceful);
-        }
+	static class Clear extends EntityRandomizerAction {
+		Clear(boolean peaceful) {
+			super(peaceful);
+		}
 
-        @Override
-        public void apply() {
-            if (this.peaceful) {
-                WorldHelper.clearPeacefuls();
-            } else {
-                WorldHelper.clearMobs();
-            }
-        }
+		@Override
+		public void apply() {
+			if (this.peaceful) {
+				WorldHelper.clearPeacefuls();
+			} else {
+				WorldHelper.clearMobs();
+			}
+		}
 
-        @Override
-        public String describe() {
-            return "Cleared the " + (this.peaceful ? "peaceful" : "hostile") + " Philosopher Stone Entity Randomizer.";
-        }
-    }
+		@Override
+		public String describe() {
+			return "Cleared the " + (this.peaceful ? "peaceful" : "hostile") + " Philosopher Stone Entity Randomizer.";
+		}
+	}
 }
