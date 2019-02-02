36d6d27bfa681a760301d0c429ab09610100ceb0
James <jamestompkins@hotmail.co.uk>
2019-02-02 13:08:50 -0600

    Removed unused imports in MK2 condenser

--- src/main/java/moze_intel/projecte/gameObjs/gui/GUICondenserMK2.java
+++ src/main/java/moze_intel/projecte/gameObjs/gui/GUICondenserMK2.java
@@ -3,16 +3,8 @@ package moze_intel.projecte.gameObjs.gui;
 import moze_intel.projecte.PECore;
 import moze_intel.projecte.gameObjs.container.CondenserMK2Container;
 import moze_intel.projecte.gameObjs.tiles.CondenserMK2Tile;
-import moze_intel.projecte.utils.Constants;
-import moze_intel.projecte.utils.TransmutationEMCFormatter;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.inventory.GuiContainer;
-import net.minecraft.client.renderer.GlStateManager;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
-
-import java.util.Arrays;
 
 public class GUICondenserMK2 extends GUICondenser
 {
