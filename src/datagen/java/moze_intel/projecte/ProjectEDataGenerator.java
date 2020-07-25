7dad1d24887ed6b72ddd39a256e997b1c668d99d
pupnewfster <richard@freimer.com>
2020-07-25 14:01:18 -0400

    Setup basic structure to allow for transitioning things over to data generators

---
+++ src/datagen/java/moze_intel/projecte/ProjectEDataGenerator.java
@@ -0,0 +1,26 @@
+package moze_intel.projecte;
+
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.client.model.generators.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+
+@EventBusSubscriber(modid = PECore.MODID, bus = Bus.MOD)
+public class ProjectEDataGenerator {
+
+	@SubscribeEvent
+	public static void gatherData(GatherDataEvent event) {
+		DataGenerator gen = event.getGenerator();
+		ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+		if (event.includeClient()) {
+			//Client side data generators
+
+		}
+		if (event.includeServer()) {
+			//Server side data generators
+
+		}
+	}
+}
