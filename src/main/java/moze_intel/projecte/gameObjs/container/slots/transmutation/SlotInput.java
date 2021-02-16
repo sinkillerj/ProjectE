5247a590358cc2dcd6fef3c7ce62fed3d735e90f
Sara Freimer <sara@freimer.com>
2021-02-16 14:39:27 -0500

    Update deps in dev and fix some issues with transmutation input slots not properly updating when interacting with things like klein stars #2092

--- src/main/java/moze_intel/projecte/gameObjs/container/slots/transmutation/SlotInput.java
+++ src/main/java/moze_intel/projecte/gameObjs/container/slots/transmutation/SlotInput.java
@@ -2,9 +2,11 @@ package moze_intel.projecte.gameObjs.container.slots.transmutation;
 
 import java.math.BigInteger;
 import java.util.Collections;
+import java.util.Optional;
 import javax.annotation.Nonnull;
 import moze_intel.projecte.api.ProjectEAPI;
 import moze_intel.projecte.api.capabilities.IKnowledgeProvider.TargetUpdateType;
+import moze_intel.projecte.api.capabilities.item.IItemEmcHolder;
 import moze_intel.projecte.api.capabilities.tile.IEmcStorage.EmcAction;
 import moze_intel.projecte.gameObjs.container.inventory.TransmutationInventory;
 import moze_intel.projecte.gameObjs.container.slots.SlotPredicates;
@@ -49,14 +51,31 @@ public class SlotInput extends SlotItemHandler {
 				if (EMCHelper.doesItemHaveEmc(stack)) {
 					inv.handleKnowledge(stack);
 				}
-				stack.getCapability(ProjectEAPI.EMC_HOLDER_ITEM_CAPABILITY).ifPresent(emcHolder -> {
-					long shrunkenAvailableEMC = MathUtils.clampToLong(inv.getAvailableEmc());
+				Optional<IItemEmcHolder> capability = stack.getCapability(ProjectEAPI.EMC_HOLDER_ITEM_CAPABILITY).resolve();
+				if (capability.isPresent()) {
+					IItemEmcHolder emcHolder = capability.get();
+					//Get the emc that the inventory has that is not in any stars
+					long shrunkenAvailableEMC = MathUtils.clampToLong(inv.provider.getEmc());
+					//try to insert it
 					long actualInserted = emcHolder.insertEmc(stack, shrunkenAvailableEMC, EmcAction.EXECUTE);
 					if (actualInserted > 0) {
+						//if we actually managed to insert some sync the slots changed, but don't update targets
+						// as that will be done by removing the emc and syncing how much is stored there
 						inv.syncChangedSlots(Collections.singletonList(getSlotIndex()), TargetUpdateType.NONE);
 						inv.removeEmc(BigInteger.valueOf(actualInserted));
+					} else if (emcHolder.getStoredEmc(stack) > 0) {
+						//If we didn't manage to insert any into our star, and we do have emc stored
+						// update the targets
+						inv.syncChangedSlots(Collections.singletonList(getSlotIndex()), TargetUpdateType.ALL);
+					} else {
+						//If we didn't manage to insert any into our star, and we don't have any emc stored
+						// don't bother updating the targets
+						inv.syncChangedSlots(Collections.singletonList(getSlotIndex()), TargetUpdateType.NONE);
 					}
-				});
+				} else {
+					//Update the fact the slots changed but don't bother updating targets
+					inv.syncChangedSlots(Collections.singletonList(getSlotIndex()), TargetUpdateType.NONE);
+				}
 			}
 		}
 	}
