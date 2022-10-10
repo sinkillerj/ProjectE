7675d30a5ec9d0fc2e66c0ad266d094764f3c43d
Donovan Daniels <hewwo@yiff.rocks>
2022-10-10 11:37:17 -0500

    tabs

--- src/main/java/moze_intel/projecte/network/commands/EMCCMD.java
+++ src/main/java/moze_intel/projecte/network/commands/EMCCMD.java
@@ -23,115 +23,115 @@ import java.math.BigInteger;
 import java.util.Optional;
 
 public class EMCCMD {
-    private enum ActionType {
-        ADD,
-        REMOVE,
-        SET,
-        GET,
-        TEST
-    }
-    public static ArgumentBuilder<CommandSourceStack, ?> register(CommandBuildContext context) {
-         return Commands.literal("emc")
-            .then(Commands.literal("add")
-                .requires(PEPermissions.COMMAND_EMC_ADD)
-                .then(executeWithParameters(ActionType.ADD))
-            )
-            .then(Commands.literal("remove")
-                .requires(PEPermissions.COMMAND_EMC_REMOVE)
-                .then(executeWithParameters(ActionType.REMOVE))
-            )
-            .then(Commands.literal("set")
-                .requires(PEPermissions.COMMAND_EMC_SET)
-                    .then(executeWithParameters(ActionType.SET))
-            )
-            .then(Commands.literal("test")
-                .requires(PEPermissions.COMMAND_EMC_TEST)
-                .then(executeWithParameters(ActionType.TEST))
-            )
-            .then(Commands.literal("get")
-                .requires(PEPermissions.COMMAND_EMC_GET)
-                .then(Commands.argument("player", EntityArgument.player())
-                    .executes((ctx) -> handle(ctx, ActionType.GET))
-                )
-            );
-    }
+	private enum ActionType {
+		ADD,
+		REMOVE,
+		SET,
+		GET,
+		TEST
+	}
+	public static ArgumentBuilder<CommandSourceStack, ?> register(CommandBuildContext context) {
+		return Commands.literal("emc")
+			.then(Commands.literal("add")
+				.requires(PEPermissions.COMMAND_EMC_ADD)
+				.then(executeWithParameters(ActionType.ADD))
+			)
+			.then(Commands.literal("remove")
+				.requires(PEPermissions.COMMAND_EMC_REMOVE)
+				.then(executeWithParameters(ActionType.REMOVE))
+			)
+			.then(Commands.literal("set")
+				.requires(PEPermissions.COMMAND_EMC_SET)
+				.then(executeWithParameters(ActionType.SET))
+			)
+			.then(Commands.literal("test")
+				.requires(PEPermissions.COMMAND_EMC_TEST)
+				.then(executeWithParameters(ActionType.TEST))
+			)
+			.then(Commands.literal("get")
+				.requires(PEPermissions.COMMAND_EMC_GET)
+				.then(Commands.argument("player", EntityArgument.player())
+					.executes((ctx) -> handle(ctx, ActionType.GET))
+				)
+			);
+	}
 
-    private static ArgumentBuilder<CommandSourceStack, ?> executeWithParameters(ActionType actionType) {
-        return Commands.argument("player", EntityArgument.player())
-            .then(Commands.argument("value", StringArgumentType.string())
-                .executes(ctx -> handle(ctx, actionType))
-            );
-    }
-    private static MutableComponent formatEMC(BigInteger emc) {
-        return TextComponentUtil.build(ChatFormatting.GRAY, TransmutationEMCFormatter.formatEMC(emc));
-    }
+	private static ArgumentBuilder<CommandSourceStack, ?> executeWithParameters(ActionType actionType) {
+		return Commands.argument("player", EntityArgument.player())
+			.then(Commands.argument("value", StringArgumentType.string())
+				.executes(ctx -> handle(ctx, actionType))
+			);
+	}
+	private static MutableComponent formatEMC(BigInteger emc) {
+		return TextComponentUtil.build(ChatFormatting.GRAY, TransmutationEMCFormatter.formatEMC(emc));
+	}
 
-    private static int handle(CommandContext<CommandSourceStack> ctx, ActionType action) throws CommandSyntaxException {
-        ServerPlayer player = EntityArgument.getPlayer(ctx, "player");
-        Optional<IKnowledgeProvider> cap = player.getCapability(PECapabilities.KNOWLEDGE_CAPABILITY).resolve();
-        if (cap.isEmpty()) {
-            ctx.getSource().sendFailure(PELang.COMMAND_PROVIDER_FAIL.translateColored(ChatFormatting.RED));
-            return 0;
-        }
-        IKnowledgeProvider provider = cap.get();
-        if (action == ActionType.GET) {
-            ctx.getSource().sendSuccess(PELang.COMMAND_EMC_GET_SUCCESS.translate(player.getDisplayName(), formatEMC(provider.getEmc())), true);
-            return 1;
-        }
-        String val = StringArgumentType.getString(ctx, "value");
-        @Nullable BigInteger value = null;
+	private static int handle(CommandContext<CommandSourceStack> ctx, ActionType action) throws CommandSyntaxException {
+		ServerPlayer player = EntityArgument.getPlayer(ctx, "player");
+		Optional<IKnowledgeProvider> cap = player.getCapability(PECapabilities.KNOWLEDGE_CAPABILITY).resolve();
+		if (cap.isEmpty()) {
+			ctx.getSource().sendFailure(PELang.COMMAND_PROVIDER_FAIL.translateColored(ChatFormatting.RED));
+			return 0;
+		}
+		IKnowledgeProvider provider = cap.get();
+		if (action == ActionType.GET) {
+			ctx.getSource().sendSuccess(PELang.COMMAND_EMC_GET_SUCCESS.translate(player.getDisplayName(), formatEMC(provider.getEmc())), true);
+			return 1;
+		}
+		String val = StringArgumentType.getString(ctx, "value");
+		@Nullable BigInteger value = null;
 
-        try {
-            value = new BigInteger(val);
-            if (value.compareTo(BigInteger.ZERO) < 0) {
-                switch (action) {
-                    case ADD, REMOVE -> {
-                        action = action == ActionType.ADD ? ActionType.REMOVE : ActionType.ADD;
-                        value = value.abs();
-                    }
-                    case SET, TEST -> {
-                        value = null;
-                    }
-                }
-            }
-        } catch (NumberFormatException ignore) {
-        }
-        if(value == null) {
-            ctx.getSource().sendFailure(PELang.COMMAND_EMC_INVALID.translateColored(ChatFormatting.RED, val));
-            return 0;
-        }
+		try {
+			value = new BigInteger(val);
+			if (value.compareTo(BigInteger.ZERO) < 0) {
+				switch (action) {
+					case ADD, REMOVE -> {
+						action = action == ActionType.ADD ? ActionType.REMOVE : ActionType.ADD;
+						value = value.abs();
+					}
+					case SET, TEST -> {
+						value = null;
+					}
+				}
+			}
+		} catch (NumberFormatException ignore) {
+		}
+		if(value == null) {
+			ctx.getSource().sendFailure(PELang.COMMAND_EMC_INVALID.translateColored(ChatFormatting.RED, val));
+			return 0;
+		}
 
-        BigInteger newEMC = provider.getEmc();
-        switch(action) {
-            case ADD -> {
-                newEMC = newEMC.add(value);
-                ctx.getSource().sendSuccess(PELang.COMMAND_EMC_ADD_SUCCESS.translate(formatEMC(value), player.getDisplayName(), formatEMC(newEMC)), true);
-            }
-            case REMOVE -> {
-                newEMC = newEMC.subtract(value);
-                if (newEMC.compareTo(BigInteger.ZERO) < 0) {
-                    ctx.getSource().sendFailure(PELang.COMMAND_EMC_NEGATIVE.translateColored(ChatFormatting.RED, formatEMC(value), player.getDisplayName()));
-                    return 0;
-                }
-                ctx.getSource().sendSuccess(PELang.COMMAND_EMC_REMOVE_SUCCESS.translate(formatEMC(value), player.getDisplayName(), formatEMC(newEMC)), true);
-            }
-            case SET -> {
-                newEMC = value;
-                ctx.getSource().sendSuccess(PELang.COMMAND_EMC_SET_SUCCESS.translate(player.getDisplayName(), formatEMC(value)), true);
-            }
-            case TEST -> {
-                if(newEMC.compareTo(value) >= 0) {
-                    ctx.getSource().sendSuccess(PELang.COMMAND_EMC_TEST_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName(), formatEMC(value)), true);
-                    return 1;
-                } else {
-                    ctx.getSource().sendFailure(PELang.COMMAND_EMC_TEST_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName(), formatEMC(value)));
-                    return 0;
-                }
-            }
-        }
+		BigInteger newEMC = provider.getEmc();
+		switch(action) {
+			case ADD -> {
+				newEMC = newEMC.add(value);
+				ctx.getSource().sendSuccess(PELang.COMMAND_EMC_ADD_SUCCESS.translate(formatEMC(value), player.getDisplayName(), formatEMC(newEMC)), true);
+			}
+			case REMOVE -> {
+				newEMC = newEMC.subtract(value);
+				if (newEMC.compareTo(BigInteger.ZERO) < 0) {
+					ctx.getSource().sendFailure(PELang.COMMAND_EMC_NEGATIVE.translateColored(ChatFormatting.RED, formatEMC(value), player.getDisplayName()));
+					return 0;
+				}
+				ctx.getSource().sendSuccess(PELang.COMMAND_EMC_REMOVE_SUCCESS.translate(formatEMC(value), player.getDisplayName(), formatEMC(newEMC)), true);
+			}
+			case SET -> {
+				newEMC = value;
+				ctx.getSource().sendSuccess(PELang.COMMAND_EMC_SET_SUCCESS.translate(player.getDisplayName(), formatEMC(value)), true);
+			}
+			case TEST -> {
+				if(newEMC.compareTo(value) >= 0) {
+					ctx.getSource().sendSuccess(PELang.COMMAND_EMC_TEST_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName(), formatEMC(value)), true);
+					return 1;
+				} else {
+					ctx.getSource().sendFailure(PELang.COMMAND_EMC_TEST_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName(), formatEMC(value)));
+					return 0;
+				}
+			}
+		}
 
-        provider.setEmc(newEMC);
-        provider.syncEmc(player);
-        return 1;
-    }
+		provider.setEmc(newEMC);
+		provider.syncEmc(player);
+		return 1;
+	}
 }
--- src/main/java/moze_intel/projecte/network/commands/KnowledgeCMD.java
+++ src/main/java/moze_intel/projecte/network/commands/KnowledgeCMD.java
@@ -23,100 +23,100 @@ import net.minecraft.world.item.ItemStack;
 import java.util.Optional;
 
 public class KnowledgeCMD {
-    private enum ActionType {
-        LEARN,
-        UNLEARN,
-        CLEAR,
-        TEST
-    }
-    public static ArgumentBuilder<CommandSourceStack, ?> register(CommandBuildContext context) {
-        return Commands.literal("knowledge")
-            .then(Commands.literal("clear")
-                .requires(PEPermissions.COMMAND_KNOWLEDGE_CLEAR)
-                .then(Commands.argument("player", EntityArgument.player())
-                    .executes((ctx) -> handle(ctx, ActionType.CLEAR))
-                )
-            )
-            .then(Commands.literal("learn")
-                .requires(PEPermissions.COMMAND_KNOWLEDGE_LEARN)
-                .then(executeWithParameters(ActionType.LEARN, context))
-            )
-            .then(Commands.literal("unlearn")
-                .requires(PEPermissions.COMMAND_KNOWLEDGE_UNLEARN)
-                .then(executeWithParameters(ActionType.UNLEARN, context))
-            )
-            .then(Commands.literal("test")
-                .requires(PEPermissions.COMMAND_KNOWLEDGE_TEST)
-                    .then(executeWithParameters(ActionType.TEST, context))
-            );
-    }
+	private enum ActionType {
+		LEARN,
+		UNLEARN,
+		CLEAR,
+		TEST
+	}
+	public static ArgumentBuilder<CommandSourceStack, ?> register(CommandBuildContext context) {
+		return Commands.literal("knowledge")
+			.then(Commands.literal("clear")
+				.requires(PEPermissions.COMMAND_KNOWLEDGE_CLEAR)
+				.then(Commands.argument("player", EntityArgument.player())
+					.executes((ctx) -> handle(ctx, ActionType.CLEAR))
+				)
+			)
+			.then(Commands.literal("learn")
+				.requires(PEPermissions.COMMAND_KNOWLEDGE_LEARN)
+				.then(executeWithParameters(ActionType.LEARN, context))
+			)
+			.then(Commands.literal("unlearn")
+				.requires(PEPermissions.COMMAND_KNOWLEDGE_UNLEARN)
+				.then(executeWithParameters(ActionType.UNLEARN, context))
+			)
+			.then(Commands.literal("test")
+				.requires(PEPermissions.COMMAND_KNOWLEDGE_TEST)
+				.then(executeWithParameters(ActionType.TEST, context))
+			);
+	}
 
-    private static ArgumentBuilder<CommandSourceStack, ?> executeWithParameters(ActionType actionType, CommandBuildContext context) {
-        return Commands.argument("player", EntityArgument.player())
-                .then(Commands.argument("item", ItemArgument.item(context))
-                .executes(ctx -> handle(ctx, actionType))
-            );
-    }
+	private static ArgumentBuilder<CommandSourceStack, ?> executeWithParameters(ActionType actionType, CommandBuildContext context) {
+		return Commands.argument("player", EntityArgument.player())
+			.then(Commands.argument("item", ItemArgument.item(context))
+				.executes(ctx -> handle(ctx, actionType))
+			);
+	}
 
-    private static int handle(CommandContext<CommandSourceStack> ctx, ActionType action) throws CommandSyntaxException {
-        ServerPlayer player = EntityArgument.getPlayer(ctx, "player");
-        Optional<IKnowledgeProvider> cap = player.getCapability(PECapabilities.KNOWLEDGE_CAPABILITY).resolve();
-        if (cap.isEmpty()) {
-            ctx.getSource().sendFailure(PELang.COMMAND_PROVIDER_FAIL.translateColored(ChatFormatting.RED));
-            return 0;
-        }
-        IKnowledgeProvider provider = cap.get();
-        if (action == ActionType.CLEAR) {
-            if (provider.getKnowledge().size() == 0) {
-                ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_CLEAR_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName()));
-                return 0;
-            }
-            provider.clearKnowledge();
-            provider.sync(player);
-            ctx.getSource().sendSuccess(PELang.COMMAND_KNOWLEDGE_CLEAR_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName()), true);
-            return 1;
-        }
+	private static int handle(CommandContext<CommandSourceStack> ctx, ActionType action) throws CommandSyntaxException {
+		ServerPlayer player = EntityArgument.getPlayer(ctx, "player");
+		Optional<IKnowledgeProvider> cap = player.getCapability(PECapabilities.KNOWLEDGE_CAPABILITY).resolve();
+		if (cap.isEmpty()) {
+			ctx.getSource().sendFailure(PELang.COMMAND_PROVIDER_FAIL.translateColored(ChatFormatting.RED));
+			return 0;
+		}
+		IKnowledgeProvider provider = cap.get();
+		if (action == ActionType.CLEAR) {
+			if (provider.getKnowledge().size() == 0) {
+				ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_CLEAR_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName()));
+				return 0;
+			}
+			provider.clearKnowledge();
+			provider.sync(player);
+			ctx.getSource().sendSuccess(PELang.COMMAND_KNOWLEDGE_CLEAR_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName()), true);
+			return 1;
+		}
 
-        ItemStack item = new ItemStack(ItemArgument.getItem(ctx, "item").getItem());
+		ItemStack item = new ItemStack(ItemArgument.getItem(ctx, "item").getItem());
 
-        if (!EMCHelper.doesItemHaveEmc(item)) {
-            ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_INVALID.translateColored(ChatFormatting.RED, item.getDisplayName()));
-            return 0;
-        }
+		if (!EMCHelper.doesItemHaveEmc(item)) {
+			ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_INVALID.translateColored(ChatFormatting.RED, item.getDisplayName()));
+			return 0;
+		}
 
-        switch (action) {
-            case LEARN -> {
-                if (provider.hasKnowledge(item)) {
-                    ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_LEARN_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName(), item.getDisplayName()));
-                    return 0;
-                } else {
-                    provider.addKnowledge(item);
-                    provider.sync(player);
-                    ctx.getSource().sendSuccess(PELang.COMMAND_KNOWLEDGE_LEARN_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName(), item.getDisplayName()), true);
-                }
-            }
-            case UNLEARN -> {
-                if (!provider.hasKnowledge(item)) {
-                    ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_UNLEARN_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName(), item.getDisplayName()));
-                    return 0;
-                } else {
-                    provider.removeKnowledge(item);
-                    provider.sync(player);
-                    ctx.getSource().sendSuccess(PELang.COMMAND_KNOWLEDGE_UNLEARN_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName(), item.getDisplayName()), true);
-                }
-            }
-            case TEST -> {
-                if (provider.hasKnowledge(item)) {
-                    ctx.getSource().sendSuccess(PELang.COMMAND_KNOWLEDGE_TEST_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName(), item.getDisplayName()), true);
-                    return 1;
-                } else {
-                    ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_TEST_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName(), item.getDisplayName()));
-                    return 0;
-                }
-            }
-        }
-        provider.syncKnowledgeChange(player, NBTManager.getPersistentInfo(ItemInfo.fromStack(item)), true);
+		switch (action) {
+			case LEARN -> {
+				if (provider.hasKnowledge(item)) {
+					ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_LEARN_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName(), item.getDisplayName()));
+					return 0;
+				} else {
+					provider.addKnowledge(item);
+					provider.sync(player);
+					ctx.getSource().sendSuccess(PELang.COMMAND_KNOWLEDGE_LEARN_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName(), item.getDisplayName()), true);
+				}
+			}
+			case UNLEARN -> {
+				if (!provider.hasKnowledge(item)) {
+					ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_UNLEARN_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName(), item.getDisplayName()));
+					return 0;
+				} else {
+					provider.removeKnowledge(item);
+					provider.sync(player);
+					ctx.getSource().sendSuccess(PELang.COMMAND_KNOWLEDGE_UNLEARN_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName(), item.getDisplayName()), true);
+				}
+			}
+			case TEST -> {
+				if (provider.hasKnowledge(item)) {
+					ctx.getSource().sendSuccess(PELang.COMMAND_KNOWLEDGE_TEST_SUCCESS.translateColored(ChatFormatting.GREEN, player.getDisplayName(), item.getDisplayName()), true);
+					return 1;
+				} else {
+					ctx.getSource().sendFailure(PELang.COMMAND_KNOWLEDGE_TEST_FAIL.translateColored(ChatFormatting.RED, player.getDisplayName(), item.getDisplayName()));
+					return 0;
+				}
+			}
+		}
+		provider.syncKnowledgeChange(player, NBTManager.getPersistentInfo(ItemInfo.fromStack(item)), true);
 
-        return 1;
-    }
+		return 1;
+	}
 }
